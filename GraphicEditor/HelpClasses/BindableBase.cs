1840ec3891f32ff1ccabc7b0761a378ec016462b
lemara98 <mile.knezevic98@gmail.com>
2020-10-29 11:58:47 +0100

    [#1-1] Create App Architecture

---
+++ GraphicEditor/HelpClasses/BindableBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicEditor.HelpClasses
+{
+    /// <summary>
+    /// This class will be inherited by every ViewModel class.
+    /// Its purpose is for enabling the dynamic change of values in the view of binded class properties.
+    /// </summary>
+    public class BindableBase : INotifyPropertyChanged
+    {
+
+        protected virtual void SetProperty<T>(ref T member, T val,
+           [CallerMemberName] string propertyName = null)
+        {
+            if (object.Equals(member, val)) return;
+
+            member = val;
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+}
---
+++ GraphicEditor/ViewModel/BuildingMapUserControlViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GraphicEditor.HelpClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicEditor.ViewModel
+{
+    public class BuildingMapUserControlViewModel : BindableBase
+    {
+
+    }
+}
