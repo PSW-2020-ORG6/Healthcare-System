554e94b96f6b9f884cb400cd1eb5f994d2b2564c
lemara98 <mile.knezevic98@gmail.com>
2020-10-29 11:58:47 +0100

    [#1-1] Create App Architecture

---
+++ GraphicEditor/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using GraphicEditor.HelpClasses;
+using GraphicEditor.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicEditor
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MyICommand<string> NavCommand { get; private set; }
+        private BuildingMapUserControlViewModel _buildingMap = new BuildingMapUserControlViewModel();
+        private HospitalMapUserControlViewModel _hospitalMap = new HospitalMapUserControlViewModel();
+        private LoginUserControlViewModel _loginPage = new LoginUserControlViewModel();
+        private BindableBase currentViewModel;
+
+        public MainWindowViewModel()
+        {
+            NavCommand = new MyICommand<string>(OnNav);
+            CurrentViewModel = _hospitalMap;
+        }
+
+        public BindableBase CurrentViewModel
+        {
+            get { return currentViewModel; }
+            set
+            {
+                SetProperty(ref currentViewModel, value);
+            }
+        }
+
+        private void OnNav(string destination)
+        {
+            switch (destination)
+            {
+                case "map":
+                    CurrentViewModel = _hospitalMap;
+                    break;
+                case "login":
+                    CurrentViewModel = _loginPage;
+                    break;
+            }
+        }
+    }
+}
---
+++ GraphicEditor/ViewModel/HospitalMapUserControlViewModel.cs
@@ -0,0 +1,23 @@
+﻿using GraphicEditor.HelpClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicEditor.ViewModel
+{
+    public class HospitalMapUserControlViewModel : BindableBase
+    {
+        private string _text = "AutoHook WORKS!!!!";
+
+        public String RandomText
+        {
+            get => _text;
+            set
+            {
+                OnPropertyChanged(RandomText);
+            }
+        }
+    }
+}
