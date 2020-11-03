41e94cbb01514fcaad71b9cf0b4e1850cfdb0c85
TanjaaD <ta.drcelic@gmail.com>
2020-11-03 17:14:34 +0100

    [#W-1.5] Create Repository for DB feedback

---
+++ project-generated-code-backend/Backend/Repository/FeedbackRepository.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Repository;
+using Model.Blog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace health_clinic_class_diagram.Backend.Repository
+{
+    public interface FeedbackRepository : GenericRepository<Feedback>
+    {
+    }
+}
