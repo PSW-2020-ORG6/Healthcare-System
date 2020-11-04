9f7793bb19113247353110c5326cb534b073f34e
Repovic_A <aleksarep@uns.ac.rs>
2020-11-04 22:45:02 +0100

    vue router added

---
+++ WebApplication/wwwroot/app/patient.js
@@ -0,0 +1,13 @@
+﻿Vue.component("patient", {
+    data: function () {
+        return {
+            test: "AKO SE OVO PRIKAZE RUTER JE USPEO PACIJENT"
+        }
+    },
+    template: `
+        <div>
+	     {{test}}    		
+	
+    </div>
+	`
+});
