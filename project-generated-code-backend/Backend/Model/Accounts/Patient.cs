26034716debae077915ad7fa4eedcd25a2c202e6
stefanwert <58173887+stefanwert@users.noreply.github.com>
2020-11-05 13:11:15 +0100

    [#GEA] Added controller and service for login

--- project-generated-code-backend/Backend/Model/Accounts/Patient.cs
+++ project-generated-code-backend/Backend/Model/Accounts/Patient.cs
@@ -16,40 +16,35 @@ namespace Model.Accounts
         private String parentName;
         private String gender;
         private bool guest;
-        private string password;
 
         public Patient(string name, string surname, string id, DateTime dateOfBirth, string contact, string email, Address address, string parentName, string gender, string password, bool isGuest = false)
-            : base(Guid.NewGuid().ToString(), name, surname, id, dateOfBirth, contact, email, address)
+            : base(Guid.NewGuid().ToString(), name, surname, id, dateOfBirth, contact, email, address,password)
         {
             this.parentName = parentName;
             this.gender = gender;
             this.Guest = isGuest;
-            this.password = password;
         }
 
         [JsonConstructor]
         public Patient(string serialNumber, string name, string surname, string id, DateTime dateOfBirth, string contact, string email, Address address, string parentName, string gender, string password, bool isGuest = false)
-            : base(serialNumber, name, surname, id, dateOfBirth, contact, email, address)
+            : base(serialNumber, name, surname, id, dateOfBirth, contact, email, address,password)
         {
             this.parentName = parentName;
             this.gender = gender;
             this.Guest = isGuest;
-            this.password = password;
         }
 
-        public Patient(PatientDTO patientDTO) : base(Guid.NewGuid().ToString(), patientDTO.Name, patientDTO.Surname, patientDTO.Id, patientDTO.DateOfBirth, patientDTO.Contact, patientDTO.Email, patientDTO.Address)
+        public Patient(PatientDTO patientDTO) : base(Guid.NewGuid().ToString(), patientDTO.Name, patientDTO.Surname, patientDTO.Id, patientDTO.DateOfBirth, patientDTO.Contact, patientDTO.Email, patientDTO.Address,patientDTO.Password)
         {
             this.parentName = patientDTO.ParentName;
             this.gender = patientDTO.Gender;
             this.Guest = patientDTO.IsGuest;
-            this.password = patientDTO.Password;
             Console.WriteLine(Guest);
         }
 
         public string ParentName { get => parentName; }
         public string Gender { get => gender; }
         public bool Guest { get => guest; set => guest = value; }
-        public string Password { get => password; }
 
         public override string ToString()
         {
