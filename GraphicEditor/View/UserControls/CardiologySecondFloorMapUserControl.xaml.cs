f2e3195319ffae4096c547c17be5a21eeb1e270b
Ana Atanackovic <anaatanackovic98@gmail.com>
2020-11-05 01:24:59 +0100

    [#GEA] Created CardiologySecondFloor and style changed

---
+++ GraphicEditor/View/UserControls/CardiologySecondFloorMapUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace GraphicEditor.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for CardiologySecondFloorMapUserControl.xaml
+    /// </summary>
+    public partial class CardiologySecondFloorMapUserControl : UserControl
+    {
+        public CardiologySecondFloorMapUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
