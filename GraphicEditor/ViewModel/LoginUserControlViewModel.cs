3cc2e94eb35005dde8b2cbf669a28fe5335244c1
stefanwert <58173887+stefanwert@users.noreply.github.com>
2020-11-05 13:11:15 +0100

    [#GEA] Added controller and service for login

--- GraphicEditor/ViewModel/LoginUserControlViewModel.cs
+++ GraphicEditor/ViewModel/LoginUserControlViewModel.cs
@@ -1,8 +1,49 @@
 ﻿using GraphicEditor.HelpClasses;
+using System;
+using System.Security;
+using System.Windows.Controls;
+using health_clinic_class_diagram;
+using health_clinic_class_diagram.Backend.Controller;
+using health_clinic_class_diagram.Backend.Model;
+using health_clinic_class_diagram.Backend.Model.Util;
 
 namespace GraphicEditor.ViewModel
 {
     public class LoginUserControlViewModel : BindableBase
     {
+        public MyICommand<PasswordBox> PasCommand { get; private set; }
+        private string _userName = "asd";
+        private string _pass = "asdasdad";
+        private HospitalLogInController hospitalLogInController;
+        public string UserName
+        {
+            get => _userName;
+            set
+            {
+                SetProperty(ref _userName, value);
+            }
+        }
+
+        public string Password
+        {
+            get => _pass;
+            set
+            {
+                OnPropertyChanged(_pass);
+            }
+        }
+        public LoginUserControlViewModel()
+        {
+            PasCommand = new MyICommand<PasswordBox>(checkLogin);
+            hospitalLogInController = new HospitalLogInController();
+        }
+
+        private void checkLogin(PasswordBox pass)
+        {
+            //repository is not implemented yet
+            //Console.WriteLine(hospitalLogInController.GetUserType(UserName,pass.Password));
+        }
+
+
     }
 }
