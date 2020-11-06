7a1e7f8211572fd584107da2eb8c42717c5a990f
Repovic_A <aleksarep@uns.ac.rs>
2020-11-06 18:55:17 +0100

    [#WA-1.1] (fix) Method for adding new feedback,backend and frontend

--- WebApplication/wwwroot/app/patient.js
+++ WebApplication/wwwroot/app/patient.js
@@ -9,8 +9,8 @@
 				approved: false,
 				date: new Date().now,
 				user: "TODO",
-				patientId: "TODO7",
-				serialNumber: "TODO7"
+				patientId: "TODO",
+				serialNumber: "TODO"
 			}
 		}
 	},
@@ -41,25 +41,21 @@
 		<div class="modal fade" tabindex="-1" role="dialog" id="feedbackModal">
 			<div class="modal-dialog" role="document">
 				<div class="modal-content">
-					<div class="modal-header">
-						<h5 class="modal-title">Leave Feedback</h5>
+					<div class="modal-header" id="feedbackModalHeader">
+						<h5 class="modal-title">Ostavite recenziju</h5>
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 							<span aria-hidden="true">&times;</span>
 						</button>
 					</div>
-					<div class="modal-body">
+					<div class="modal-body" id="feedbackModalBody">
 
 						<label>Enter your comment here:</label>
 						<input type="text" class="form-control" v-model="feedback.text">
 
-						<label id="ratingLabel">Rate us</label>
-						<div class="rating">
-							<input type="radio" name="rating" value="5" id="5"><label for="5">☆</label> <input type="radio" name="rating" value="4" id="4"><label for="4">☆</label> <input type="radio" name="rating" value="3" id="3"><label for="3">☆</label> <input type="radio" name="rating" value="2" id="2"><label for="2">☆</label> <input type="radio" name="rating" value="1" id="1"><label for="1">☆</label>
-						</div>
 					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-info btn-lg " v-on:click="addNewFeedback(feedback)">Save changes</button>
-						<button type="button" class="btn btn-info btn-lg " data-dismiss="modal">Close</button>
+					<div class="modal-footer" id="feedbackModalFooter">
+						<button type="button" class="btn btn-info btn-lg " v-on:click="addNewFeedback(feedback)">Pošalji</button>
+						<button type="button" class="btn btn-info btn-lg " data-dismiss="modal">Odustani</button>
 					</div>
 				</div>
 			</div>
@@ -113,11 +109,22 @@
 	    </div>
     </div>
    </div>
+</div>
+</div>
+\
 	`,
 	methods: {
 		addNewFeedback: function (feedback) {
-			axios
-				.post("http://localhost:49900/feedback/add", feedback)
+			if (feedback.text != null) {
+				axios
+					.post("http://localhost:49900/feedback/add", feedback)
+					.then(response => {``
+						this.feedback.text = null;
+						$('#feedbackModal').modal('hide')
+					})
+			}
+			else
+				alert("Morate uneti tekst recenzije.");
 
 		},
 		DateSplit: function (date) {
