1cb8160456133d9d388242076813bbc361e1b77a
lemara98 <mile.knezevic98@gmail.com>
2020-11-07 18:58:02 +0100

    [#GEA] Floor Change Enabled

--- GraphicEditor/ViewModel/CardiologyBuildingUserControlViewModel.cs
+++ GraphicEditor/ViewModel/CardiologyBuildingUserControlViewModel.cs
@@ -10,14 +10,21 @@ namespace GraphicEditor.ViewModel
     public class CardiologyBuildingUserControlViewModel : BindableBase
     {
         private MapContentUserControlViewModel _parent;
+        string[] array = { "Basement", "Ground Floor", "First Floor", "Second Floor", "Third Floor" };
+        private List<string> _floors;
+        private string _selectedFloor;
         public MyICommand<string> NavCommand { get; private set; }
         public static CardiologyFirstFloorMapUserControlViewModel FirstFloor;
         public static CardiologySecondFloorMapUserControlViewModel SecondFloor;
         public BindableBase _floorViewModel;
 
+
+
         public CardiologyBuildingUserControlViewModel(MapContentUserControlViewModel parent)
         {
             _parent = parent;
+            _floors = new List<string>(array);
+            _selectedFloor = _floors[2];
             NavCommand = new MyICommand<string>(ChooseFloor);
             FirstFloor = new CardiologyFirstFloorMapUserControlViewModel();
             SecondFloor = new CardiologySecondFloorMapUserControlViewModel();
@@ -30,6 +37,27 @@ namespace GraphicEditor.ViewModel
             set
             {
                 SetProperty(ref _floorViewModel, value);
+            }
+        }
+
+        public List<string> Floors
+        {
+            get
+            {
+                return _floors;
+            }
+        }
+
+        public string SelectedFloor
+        {
+            get { return _selectedFloor; }
+            set
+            {
+                _selectedFloor = value;
+                String cpy = String.Copy(_selectedFloor);
+                var paramArray = cpy.Split(' ');
+                var param = paramArray[0].ToLower();
+                ChooseFloor(param);
             }
         }
 
@@ -47,10 +75,10 @@ namespace GraphicEditor.ViewModel
 
                     break;
                 case Constants.FIRST:
-
+                    FloorViewModel = FirstFloor;
                     break;
                 case Constants.SECOND:
-
+                    FloorViewModel = SecondFloor;
                     break;
                 case Constants.THIRD:
 
