c17cb6649495d83802d664ff52adca4fdf22eb63
stefanwert <58173887+stefanwert@users.noreply.github.com>
2020-11-08 20:42:06 +0100

    [#GEA] Added room information

--- GraphicEditor/ViewModel/CardiologyFirstFloorMapUserControlViewModel.cs
+++ GraphicEditor/ViewModel/CardiologyFirstFloorMapUserControlViewModel.cs
@@ -1,8 +1,22 @@
 ﻿using GraphicEditor.HelpClasses;
+using GraphicEditor.View.UserControls;
+using System;
 
 namespace GraphicEditor.ViewModel
 {
     public class CardiologyFirstFloorMapUserControlViewModel : BindableBase
     {
+        public MyICommand<string> ShowRoomCommand { get; private set; }
+
+        public CardiologyFirstFloorMapUserControlViewModel()
+        {
+            ShowRoomCommand = new MyICommand<string>(showRoom);
+        }
+
+        private void showRoom(string a)
+        {
+            (new RoomInformation()).Show();
+        }
+
     }
 }
