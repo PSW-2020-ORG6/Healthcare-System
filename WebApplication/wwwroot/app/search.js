2d331d11cfe146c32fdcec5660ea554da236f882
TanjaaD <ta.drcelic@gmail.com>
2020-11-20 18:05:43 +0100

    [#WA-2.5] Frontend for advanced search

--- WebApplication/wwwroot/app/search.js
+++ WebApplication/wwwroot/app/search.js
@@ -10,7 +10,7 @@ Vue.component("search", {
         }
     },
     template:
-        `
+    `
         <div class= "container">
             <br/><h3 class="text">Search - prescription and report</h3><br/>
             <ul class="nav nav-tabs" role="tablist">
