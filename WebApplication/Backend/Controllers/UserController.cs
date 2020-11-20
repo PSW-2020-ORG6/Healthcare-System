552507737c11e4a404d053b9e7402637f1b6d223
TanjaaD <ta.drcelic@gmail.com>
2020-11-20 18:05:43 +0100

    [#WA-2.5] Frontend for advanced search

---
+++ WebApplication/Backend/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApplication.Backend.Controllers
+{
+    /// <summary>
+    /// This class does connection with service
+    /// </summary>
+    [Microsoft.AspNetCore.Components.Route("patient")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        //private readonly PatientService patientService;
+        public UserController()
+        {
+            //this.patientService = new PatientService();
+        }
+        [HttpGet("advancedSearch")]
+        public void GetAllFeedbacks()
+        {
+        }
+    }
+}
--- WebApplication/wwwroot/app/search.js
+++ WebApplication/wwwroot/app/search.js
@@ -2,17 +2,19 @@ Vue.component("search", {
     data: function () {
         return {
             yes: false,
-            rowPrescriptio: 0,
-            rowReport: 0
+            row: 0,
+            second: false,
+            third: false,
+            fourth: false
         }
     },
     template:
-        `
+    `
         <div class= "container">
-            <br /><h3 class="text">Search</h3><br />
+            <br/><h3 class="text">Search - prescription and report</h3><br/>
             <ul class="nav nav-tabs" role="tablist">
                 <li class="nav-item">
-                    <a class="nav-link active" data-toggle="tab" href="#simpleSearch">Simple</a>
+                    <a class=" nav-link active" data-toggle="tab" href="#simpleSearch">Simple</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" data-toggle="tab" href="#advancedSearch">Advinced</a>
@@ -21,105 +23,166 @@ Vue.component("search", {
             <div>
                 <div class="tab-content">
                     <div id="simpleSearch" class="container tab-pane active"><br/>
-                        <div class="container">
-                            <div class="row">                
+                        <div class="container">              
                         </div>
-                      </div>
                     </div>
-                    <div id="advancedSearch" class="container tab-pane fade"><br/>
-                        <div class="container">
-                        <br/><h4>Prescription</h4><br/>
-                            <div class="row">
-                                 <table id="prescription" style="width: 100%">
-                                    <colgroup>
-                                       <col style="width: 20%;">
-                                       <col style="width: 50%;">
-                                       <col style="width: 30%;">
-                                    </colgroup>
-                                    <tbody>
-                                        <tr>
-                                         <td>
-                                            <select class="col">
-                                              <option disabled>Please select one</option>
-                                              <option>AND</option>
-                                              <option>OR</option>
-                                              <option>AND NOT</option>
-                                            </select>
-                                        </td>                                    
-                                         <td><input type="text" class="col"/></td>
-                                         <td>
-                                        <select class="col">
-                                          <option disabled>Please select one</option>
-                                          <option>name</option>
-                                          <option>type</option>
-                                        </select>
-                                        </td>
+                    <div id="advancedSearch" class="container tab-pane fade">
+                        <div class="container"><br/>
+                                <div class="row">
+                                    <table id="prescription" style="width: 100%">
+                                        <colgroup>
+                                           <col style="width: 20%;">
+                                           <col style="width: 40%;">
+                                           <col style="width: 35%;">
+                                           <col style="width: 5%;">
+                                        </colgroup>
+                                        <tbody>
+                                            <tr>
+                                                <td align="center"><label>Search</label></td>                                    
+                                                <td><input type="text" class="col"/></td>
+                                                <td><label>in &nbsp</label>
+                                                    <select class="col">
+                                                    <option disabled>Please select one</option>
+                                                    <option>All</option>
+                                                    <option>Medicine name</option>
+                                                    <option>Medicine type</option>
+                                                    <option>Procedure type</option>
+                                                    <option>Patient reports</option>
+                                                    <option>Specialist reports</option>
+                                                    <option>Doctor reports</option>
+                                                    </select>
+                                                 </td>
+                                                 <td></td>
+                                             </tr>
+                                        <tr v-if="second">
+                                            <td>
+                                                <select class="col">
+                                                    <option disabled>Please select one</option>
+                                                    <option>AND</option>
+                                                    <option>OR</option>
+                                                    <option>AND NOT</option>
+                                                 </select>
+                                            </td>                                    
+                                            <td><input type="text" class="col"/></td>
+                                            <td><label>in &nbsp</label>
+                                                <select class="col">
+                                                    <option disabled>Please select one</option>
+                                                    <option>All</option>
+                                                    <option>Medicine name</option>
+                                                    <option>Medicine type</option>
+                                                    <option>Procedure type</option>
+                                                    <option>Patient reports</option>
+                                                    <option>Specialist reports</option>
+                                                    <option>Doctor reports</option>
+                                                </select>
+                                            </td>
+                                            <td>
+                                                <button class="circledelete" v-on:click="DeleteRow(), second=false"><i class="fa fa-close""></i></button>                           
+                                            </td>
+                                        </tr>
+                                        <tr v-if="third">
+                                            <td>
+                                                <select class="col">
+                                                    <option disabled>Please select one</option>
+                                                    <option>AND</option>
+                                                    <option>OR</option>
+                                                    <option>AND NOT</option>
+                                                </select>
+                                            </td>                                    
+                                            <td><input type="text" class="col"/></td>
+                                            <td><label>in &nbsp</label>
+                                                <select class="col">
+                                                    <option disabled>Please select one</option>
+                                                    <option>All</option>
+                                                    <option>Medicine name</option>
+                                                    <option>Medicine type</option>
+                                                    <option>Procedure type</option>
+                                                    <option>Patient reports</option>
+                                                    <option>Specialist report</option>
+                                                    <option>Doctor reports</option>
+                                                </select>
+                                            </td>
+                                            <td>
+                                                <button class="circledelete" v-on:click="DeleteRow(), third=false"><i class="fa fa-close""></i></button>                           
+                                            </td>
+                                        </tr>
+                                        <tr v-if="fourth">
+                                            <td>
+                                                <select class="col">
+                                                    <option disabled>Please select one</option>
+                                                    <option>AND</option>
+                                                    <option>OR</option>
+                                                    <option>AND NOT</option>
+                                                </select>
+                                             </td>                                    
+                                             <td><input type="text" class="col"/></td>
+                                             <td><label>in &nbsp</label>
+                                                <select class="col">
+                                                    <option disabled>Please select one</option>
+                                                    <option>All</option>
+                                                    <option>Medicine name</option>
+                                                    <option>Medicine type</option>
+                                                    <option>Procedure type</option>
+                                                    <option>Patient reports</option>
+                                                    <option>Specialist reports</option>
+                                                    <option>Doctor reports</option>
+                                                </select>
+                                              </td>
+                                            <td>
+                                                <button class="circledelete" v-on:click="DeleteRow(), fourth=false"><i class="fa fa-close""></i></button>                           
+                                            </td>
                                        </tr>
-                                     </tbody>
-                                  </table>
+                                   </tbody>
+                                </table>
                             </div>
-                            <button v-if="rowPrescriptio<3" class="circle" v-on:click="AddRowPrescription()"><i class="fa fa-plus"></i></button>
-                            <br/><br/>
-                            <br/><h4>Report</h4><br/>
-                            <div class="row">
-                                 <table id="report" style="width: 100%">
-                                    <colgroup>
-                                       <col style="width: 20%;">
-                                       <col style="width: 50%;">
-                                       <col style="width: 30%;">
-                                    </colgroup>
-                                    <tbody>
-                                        <tr>
-                                         <td>
-                                            <select class="col">
-                                              <option disabled>Please select one</option>
-                                              <option>AND</option>
-                                              <option>OR</option>
-                                              <option>AND NOT</option>
-                                            </select>
-                                        </td>                                    
-                                         <td><input type="text" class="col"/></td>
-                                         <td>
-                                        <select class="col">
-                                          <option disabled>Please select one</option>
-                                          <option>patient</option>
-                                          <option>specialization</option>
-                                          <option>doctor</option>
-                                        </select>
-                                        </td>
-                                       </tr>
-                                     </tbody>
-                                  </table>
-                            </div>
-                            <button v-if="rowReport<3" class="circle" v-on:click="AddRowReport()"><i class="fa fa-plus"></i></button>
-                        </div>
-                         <br/><br/>
-                        <button class="btn btn-info btn-lg">Search</button>
+                            <button v-if="row<3" class="circleadd" v-on:click="AddRow()"><i class="fa fa-plus"></i></button><br/><br/>                            
+                        
+                        <input type="checkbox"/>
+                        <label>Approximate search</label>
+                        </div><br/><br/>
+                        <div class="row">
+                            <label>&nbsp&nbsp Datum od &nbsp&nbsp</label><input type="date"></input>
+                            <label>&nbsp&nbsp do &nbsp&nbsp</label><input type="date"></input>
+                        </div><br/><br/>
+                        <button class="btnSearch btn-info btn-lg">Search</button>
+                        <div class="container"><br/><hr/><br/>
+                         <div class="row">
+                            <h4>Rezultat pretrage:</h4>    
+                         </div><br/>
+                         <table style="width: 100%">
+                         <th>Recept DATUM</th>
+                         <tr>lek-cena-tip-opis</tr>
+                         <tr>opis</tr>
+                         </table><br/>
+                         <table style="width: 100%">
+                         <th>pregeld DATUM -tip</th>
+                         <tr>pacijent</tr>
+                         <tr>lekar</tr>
+                         <tr>opis</tr>
+                         </table>
+                    </div>
                     </div>
                 </div>
-            </div>
-    </div> 
+            </div><br/>
+        </div> 
 	`,
     methods: {
-        AddRowPrescription: function() {
-            var root = document.getElementById('prescription').getElementsByTagName('tbody')[0];
-            var rows = root.getElementsByTagName('tr');
-            this.yes = true;
-            var clone = this.CloneEl(rows[rows.length - 1]);
-            root.appendChild(clone);
-            this.rowPrescriptio += 1;
-        },
-        AddRowReport: function () {
-            var root = document.getElementById('report').getElementsByTagName('tbody')[0];
-            var rows = root.getElementsByTagName('tr');
-            this.yes = true;
-            var clone = this.CloneEl(rows[rows.length - 1]);
-            root.appendChild(clone);
-            this.rowReport += 1;
+        AddRow: function () {
+            if (!this.second)
+                this.second = true
+            else if (!this.third)
+                this.third = true
+            else if (!this.fourth)
+                this.fourth = true
+            this.row += 1
+
         },
         CloneEl: function(el) {
             var clo = el.cloneNode(true);
             return clo;
+        },
+        DeleteRow: function () {
+            this.row -= 1
         }
     }
 });
