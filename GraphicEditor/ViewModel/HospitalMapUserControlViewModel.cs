5fdcd8f2737a2751ef1b3657bbf45792fb083adb
Hadzija <hadzijasale@gmail.com>
2020-11-22 20:29:02 +0100

    [GEA]Adding new building

--- GraphicEditor/ViewModel/HospitalMapUserControlViewModel.cs
+++ GraphicEditor/ViewModel/HospitalMapUserControlViewModel.cs
@@ -1,18 +1,28 @@
 ﻿using GraphicEditor.HelpClasses;
+using GraphicEditor.View.Windows;
+using System.Windows.Controls;
 
 namespace GraphicEditor.ViewModel
 {
-
 
     public class HospitalMapUserControlViewModel : BindableBase
     {
         private MapContentUserControlViewModel _parent;
         public MyICommand<string> NavCommand { get; private set; }
+        public MyICommand<object> AddCommand { get; private set; }
 
         public HospitalMapUserControlViewModel(MapContentUserControlViewModel parent)
         {
             _parent = parent;
             NavCommand = new MyICommand<string>(ChooseHospital);
+            AddCommand = new MyICommand<object>(AddBuilding);
+        }
+
+        private void AddBuilding(object button)
+        {
+            Button but = (Button)button;
+            AddBuildingViewModel.GetInstance().Button = but;
+            (new AddBuilding()).ShowDialog();
         }
 
         private void ChooseHospital(string destination)
