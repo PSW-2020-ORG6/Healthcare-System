d15d804f9a0d01fd4a70f72abe3a13ce12fbb564
maja-tepavcevic <maja.tepavcevic@uns.ac.rs>
2020-11-26 14:43:50 +0100

    [#IA] Tests for register hospital on pharmacy-Correction

--- IntegrationAdaptersTests/RegisterHospitalTests.cs
+++ IntegrationAdaptersTests/RegisterHospitalTests.cs
@@ -1,24 +1,24 @@
 using System;
+using System.Collections.Generic;
 using System.Configuration;
 using IntegrationAdapters;
 using IntegrationAdapters.Models;
+using IntegrationAdapters.Repositories;
 using IntegrationAdapters.Services;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 using Xunit;
 
 namespace IntegrationAdaptersTests
 {
     public class RegisterHospitalTests
     {
-        public readonly HealthCareSystemDbContext dbContext;
+        public Mock<IApiRepository> mock;
+
         [Fact]
         public void Is_pharmacy_already_existing_on_hospital()
-        {
-            var options = new DbContextOptionsBuilder<HealthCareSystemDbContext>()
-                .UseMySql(connectionString: "server=localhost;port=3306;database=newmydb;user=root;password=root")
-                .Options;
-
-            ApiService service = new ApiService(new HealthCareSystemDbContext(options));
+        {          
+            ApiService service = new ApiService();
 
             Api a = new Api("k", "k", "k");
 
@@ -29,17 +29,51 @@ namespace IntegrationAdaptersTests
         [Fact]
         public void Is_pharmacy_not_existing_on_hospital()
         {
-            var options = new DbContextOptionsBuilder<HealthCareSystemDbContext>()
-                .UseMySql(connectionString: "server=localhost;port=3306;database=newmydb;user=root;password=root")
-                .Options;
+            ApiService service = new ApiService();
 
-            ApiService service = new ApiService(new HealthCareSystemDbContext(options));
-
-            Api a = new Api("m", "m", "m");
+            Api a = new Api("l", "l", "l");
 
             bool notExists = service.RegisterHospitalOnPharmacy(a);
 
             Assert.True(notExists);
+        }
+        [Fact]
+        public void Is_apiKey_null()
+        {
+            mock = new Mock<IApiRepository>();
+            mock.Setup(expression:t => t.GetAllApis()).Returns(new List<Api> { new Api { Key = null, PharmacyName = "name1", Url = "url1" }});
+            ApiService service = new ApiService();
+            Api api = mock.Object.GetAllApis()[0];
+            Assert.Null(service.getApiKey(api));
+        }
+        [Fact]
+        public void Is_apiKey_not_null()
+        {
+            mock = new Mock<IApiRepository>();
+            mock.Setup(expression: t => t.GetAllApis()).Returns(new List<Api> { new Api { Key = "kljuc1", PharmacyName = "name1", Url = "url1" } });
+            ApiService service = new ApiService();
+            Api api = mock.Object.GetAllApis()[0];
+            Assert.NotNull(service.getApiKey(api));
+        }
+
+        [Fact]
+        public void Find_existing_api()
+        {
+            mock = new Mock<IApiRepository>();
+            mock.Setup(expression: t => t.GetAllApis()).Returns(new List<Api> { new Api("12345678","pharmacyName","pharmacyUrl")});
+            ApiService service = new ApiService();
+            Api api = service.getApiByKey(mock.Object.GetAllApis(), "12345678");
+            Assert.NotNull(api);
+        }
+
+        [Fact]
+        public void Find_not_existing_api()
+        {
+            mock = new Mock<IApiRepository>();
+            mock.Setup(expression: t => t.GetAllApis()).Returns(new List<Api> { new Api { Key = "1234567", PharmacyName = "pharmacyName1", Url = "pharmacyUrl1" } });
+            ApiService service = new ApiService();
+            Api api = service.getApiByKey(mock.Object.GetAllApis(),"23478");
+            Assert.Null(api);
         }
     }
 }
