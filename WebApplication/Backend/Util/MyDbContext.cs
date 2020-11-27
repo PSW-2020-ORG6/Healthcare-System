243cd4334018d78685878cdf6092fa82fb74df59
Ana Atanackovic <anaatanackovic98@gmail.com>
2020-11-27 13:39:42 +0100

    [#GEA] Added data for buildings, floors and rooms

--- WebApplication/Backend/Util/MyDbContext.cs
+++ WebApplication/Backend/Util/MyDbContext.cs
@@ -39,6 +39,23 @@ namespace WebApplication.Backend.Model
                  new Patient { Name = "Jelena", Surname = "Tanjic", Id = "0002", DateOfBirth = new DateTime(2017, 1, 18), Contact = "kontakt", Password = "sifra", Address = new Address("neka adresa"), ParentName = "otac", Gender = "Zensko", Email = "email", Guest = true },
                  new Patient { Name = "Sara", Surname = "Milic", Id = "0003", DateOfBirth = new DateTime(2018, 1, 18), Contact = "kontaktMica", Password = "sifraMica", Address = new Address("neka adresaMica"), ParentName = "mama", Gender = "Zensko", Email = "emailMica", Guest = true }
             );
+            modelBuilder.Entity<RoomGEA>().HasData(
+                new RoomGEA { SerialNumber = "Room1", Id = "9001", FloorId = "1001" },
+                new RoomGEA { SerialNumber = "Room2", Id = "9002", FloorId = "1001" },
+                new RoomGEA { SerialNumber = "Room3", Id = "9003", FloorId = "1001" },
+                new RoomGEA { SerialNumber = "Room4", Id = "9004", FloorId = "1001" },
+                new RoomGEA { SerialNumber = "Room5", Id = "9005", FloorId = "1001" },
+                new RoomGEA { SerialNumber = "Operation room1", Id = "9006", FloorId = "1002" },
+                new RoomGEA { SerialNumber = "Operation room2", Id = "9007", FloorId = "1002" },
+                new RoomGEA { SerialNumber = "Store room", Id = "9008", FloorId = "1002" }
+            );
+            modelBuilder.Entity<Floor>().HasData(
+              new Floor { Id = "1001", Name = "Floor1", BuildingId = "10001" },
+              new Floor { Id = "1002", Name = "Floor2", BuildingId = "10001" }
+            );
+            modelBuilder.Entity<Building>().HasData(
+              new Building { Id = "10001", Name = "Cardiology", Color = "Orange", Shape = "Square" }
+            );
         }
     }
 }
