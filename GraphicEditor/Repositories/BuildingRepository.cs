c9f4a88089a4e5ee0e80f6d87a8d51ac901d7f33
Ana Atanackovic <anaatanackovic98@gmail.com>
2020-11-27 14:54:45 +0100

    [#GEA] Getting floors/buildings/rooms by its attributes for GEA

---
+++ GraphicEditor/Repositories/BuildingRepository.cs
@@ -0,0 +1,93 @@
+﻿using GraphicEditor.Repositories.Interfaces;
+using health_clinic_class_diagram.Backend.Model.Hospital;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace GraphicEditor.Repositories
+{
+    public class BuildingRepository : IBuildingRepository
+    {
+        private MySqlConnection connection;
+        public BuildingRepository()
+        {
+            connection = new MySqlConnection("server=localhost;port=3306;database=mydb;user=root;password=root");
+            connection.Open();
+        }
+
+        public List<Building> GetBuildings(String sqlDml)
+        {
+            MySqlCommand sqlCommand = new MySqlCommand(sqlDml, connection);
+            MySqlDataReader sqlReader = sqlCommand.ExecuteReader();
+            List<Building> resultList = new List<Building>();
+            while (sqlReader.Read())
+            {
+                Building entity = new Building();
+                entity.Id = (string)sqlReader[0];
+                entity.Name = (string)sqlReader[1];
+                entity.Color = (string)sqlReader[2];
+                entity.Shape = (string)sqlReader[3];
+                resultList.Add(entity);
+            }
+            connection.Close();
+            return resultList;
+        }
+
+        public List<Building> GetAllBuildings()
+        {
+            return GetBuildings("Select * from buildings");
+        }
+
+        public Building GetBuildingById(String sqlDml)
+        {
+            try
+            {
+                Building building = GetBuildings(sqlDml)[0];
+                return building;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public Building GetBuildingByName(String sqlDml)
+        {
+            try
+            {
+                Building building = GetBuildings(sqlDml)[1];
+                return building;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public Building GetBuildingByColor(String sqlDml)
+        {
+            try
+            {
+                Building building = GetBuildings(sqlDml)[2];
+                return building;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public Building GetBuildingByShape(String sqlDml)
+        {
+            try
+            {
+                Building building = GetBuildings(sqlDml)[3];
+                return building;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ GraphicEditor/Repositories/RoomRepository.cs
@@ -0,0 +1,79 @@
+﻿using GraphicEditor.Repositories.Interfaces;
+using health_clinic_class_diagram.Backend.Model.Hospital;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace GraphicEditor.Repositories
+{
+    public class RoomRepository : IRoomRepository
+    {
+        private MySqlConnection connection;
+        public RoomRepository()
+        {
+            connection = new MySqlConnection("server=localhost;port=3306;database=mydb;user=root;password=root");
+            connection.Open();
+        }
+
+        public List<RoomGEA> GetRooms(String sqlDml)
+        {
+            MySqlCommand sqlCommand = new MySqlCommand(sqlDml, connection);
+            MySqlDataReader sqlReader = sqlCommand.ExecuteReader();
+            List<RoomGEA> resultList = new List<RoomGEA>();
+            while (sqlReader.Read())
+            {
+                RoomGEA entity = new RoomGEA();
+                entity.SerialNumber = (string)sqlReader[0];
+                entity.Id = (string)sqlReader[1];
+                entity.FloorId = (string)sqlReader[2];
+                resultList.Add(entity);
+            }
+            connection.Close();
+            return resultList;
+        }
+
+        public List<RoomGEA> GetAllRooms()
+        {
+            return GetRooms("Select * from rooms");
+        }
+
+        public RoomGEA GetRoomById(String sqlDml)
+        {
+            try
+            {
+                RoomGEA room = GetRooms(sqlDml)[1];
+                return room;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public RoomGEA GetRoomByName(String sqlDml)
+        {
+            try
+            {
+                RoomGEA room = GetRooms(sqlDml)[0];
+                return room;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public RoomGEA GetRoomByFloorId(String sqlDml)
+        {
+            try
+            {
+                RoomGEA room = GetRooms(sqlDml)[2];
+                return room;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
