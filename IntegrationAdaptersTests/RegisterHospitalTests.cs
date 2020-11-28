d487a28fb2387541e041a9e50a19623caf45ed4c
maja-tepavcevic <maja.tepavcevic@uns.ac.rs>
2020-11-28 14:17:38 +0100

    [#IA] Tests for register hospital on pharmacy-Correction2

--- IntegrationAdaptersTests/RegisterHospitalTests.cs
+++ IntegrationAdaptersTests/RegisterHospitalTests.cs
@@ -20,49 +20,32 @@ namespace IntegrationAdaptersTests
         {          
             ApiService service = new ApiService();
 
-            Api a = new Api("k", "k", "k");
-
-            bool alreadyExists = service.RegisterHospitalOnPharmacy(a);
+            Api registerFailed = new Api("k", "k", "k");
+            bool alreadyExists = service.RegisterHospitalOnPharmacy(registerFailed);
 
             Assert.False(alreadyExists);
         }
+
         [Fact]
         public void Is_pharmacy_not_existing_on_hospital()
         {
             ApiService service = new ApiService();
 
-            Api a = new Api("l", "l", "l");
-
-            bool notExists = service.RegisterHospitalOnPharmacy(a);
+            Api registerSuccessful = new Api("l", "l", "l");
+            bool notExists = service.RegisterHospitalOnPharmacy(registerSuccessful);
 
             Assert.True(notExists);
-        }
-        [Fact]
-        public void Is_apiKey_null()
-        {
-            mock = new Mock<IApiRepository>();
-            mock.Setup(expression:t => t.GetAllApis()).Returns(new List<Api> { new Api { Key = null, PharmacyName = "name1", Url = "url1" }});
-            ApiService service = new ApiService();
-            Api api = mock.Object.GetAllApis()[0];
-            Assert.Null(service.getApiKey(api));
-        }
-        [Fact]
-        public void Is_apiKey_not_null()
-        {
-            mock = new Mock<IApiRepository>();
-            mock.Setup(expression: t => t.GetAllApis()).Returns(new List<Api> { new Api { Key = "kljuc1", PharmacyName = "name1", Url = "url1" } });
-            ApiService service = new ApiService();
-            Api api = mock.Object.GetAllApis()[0];
-            Assert.NotNull(service.getApiKey(api));
-        }
+        }   
 
         [Fact]
         public void Find_existing_api()
         {
             mock = new Mock<IApiRepository>();
             mock.Setup(expression: t => t.GetAllApis()).Returns(new List<Api> { new Api("12345678","pharmacyName","pharmacyUrl")});
+            
             ApiService service = new ApiService();
             Api api = service.getApiByKey(mock.Object.GetAllApis(), "12345678");
+
             Assert.NotNull(api);
         }
 
@@ -71,8 +54,10 @@ namespace IntegrationAdaptersTests
         {
             mock = new Mock<IApiRepository>();
             mock.Setup(expression: t => t.GetAllApis()).Returns(new List<Api> { new Api { Key = "1234567", PharmacyName = "pharmacyName1", Url = "pharmacyUrl1" } });
+            
             ApiService service = new ApiService();
             Api api = service.getApiByKey(mock.Object.GetAllApis(),"23478");
+           
             Assert.Null(api);
         }
     }
