469e4961142951069b8d55264b17e434590c7a54
milica-okiljevic <58478503+milica-okiljevic@users.noreply.github.com>
2020-11-28 14:42:59 +0100

    [IA] Receiving messege from pharmacy

--- IntegrationAdapters/Program.cs
+++ IntegrationAdapters/Program.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using IntegrationAdapters.Adapters;
 using IntegrationAdapters.Controllers;
+using IntegrationAdapters.Models;
+using IntegrationAdapters.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,6 +16,7 @@ namespace IntegrationAdapters
 {
     public class Program
     {
+        public static List<Message> Messages = new List<Message>();
 
         public static void Main(string[] args)
         {
@@ -21,9 +25,19 @@ namespace IntegrationAdapters
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .ConfigureServices((hostContext, services) =>
+                {
+
+                    Messages.ForEach((theMoney) => Console.WriteLine("amount is {0}, and type is {1}", theMoney.Text, theMoney.Timestamp));
+                    services.AddHostedService<TimerService>();
+                    services.AddHostedService<RabbitMQService>();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+
     }
 }
---
+++ IntegrationAdapters/wwwroot/js/ActionsAndBenefits.js
@@ -0,0 +1,25 @@
+﻿function drawTable(data) {
+	let table = '';
+	for (i in data) {
+		console.log('bdhkcbjkxz')
+		table += `<tr>
+			<td>`+ data[i].text + `</td>
+			<td>`+ data[i].timestamp + `</td>
+			</tr>`;
+	}
+	$('#actionsAndBenefitsTable').html(table);
+
+}	
+
+$(document).ready(function () {
+		$.get({
+			url: '../api/actionsandbenefits',
+			contentType: 'application/json',
+			success: function (data) {
+				drawTable(data);
+			},
+			error: function (message) {
+				alert("Failed")
+			}
+		});
+	});
