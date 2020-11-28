d9336707d1e37aa137990c0f50ae67eac6ac8be9
Hadzija <hadzijasale@gmail.com>
2020-11-28 17:44:10 +0100

    [#GEA]Changes to add building

---
+++ GraphicEditor/ViewModel/DBtest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace GraphicEditor.ViewModel
+{
+    class DBtest
+    {
+        public static void DatabaseConnection()
+        {
+            for(int i = 0; i < 10; i ++)
+            {
+                Console.WriteLine("Cao");
+            }
+            string connstring = @"server=localhost;port=3306;database=mydb;user=root;password=root";
+
+            MySqlConnection conn = null;
+
+            try
+            {
+                conn = new MySqlConnection(connstring);
+                conn.Open();
+
+                string query = "SELECT * FROM buildings;";
+                MySqlDataAdapter da = new MySqlDataAdapter(query, conn);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "buildings");
+                DataTable dt = ds.Tables["buildings"];
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    foreach (DataColumn col in dt.Columns)
+                    {
+                        Console.Write(row[col] + "\t");
+                    }
+
+                    Console.Write("\n");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.ToString());
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
