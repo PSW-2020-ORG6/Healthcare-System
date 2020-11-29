8f2a5e2f4582ae1440bc267784a9cc204a803228
dragana-todorovic <dragana.todorovic@uns.ac.rs>
2020-11-29 17:03:42 +0100

    [#IA] Send report using sftp protocol

---
+++ IntegrationAdapters/Models/MedicineReport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationAdapters.Models
+{
+    public class MedicineReport
+    {
+        public string Id { get; set; }
+        public virtual DateTime date { get; set; }
+        public virtual List<MedicineDosage> dosage { get; set; }  
+
+        public MedicineReport() {
+            dosage = new List<MedicineDosage>();
+        }
+
+    }
+}
