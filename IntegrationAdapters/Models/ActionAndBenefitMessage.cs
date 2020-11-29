48a37e2e63f42097ae0cd256543d94cc82513700
milica-okiljevic <58478503+milica-okiljevic@users.noreply.github.com>
2020-11-29 21:34:30 +0100

    [#IA] Receiving message from pharmacy and saving picked message to DB

--- IntegrationAdapters/Models/ActionAndBenefitMessage.cs
+++ IntegrationAdapters/Models/ActionAndBenefitMessage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,13 @@ namespace IntegrationAdapters.Models
 {
     public class ActionAndBenefitMessage
     {
+        [Key]
         public Guid ActionID { get; set; }
         public string PharmacyName { get; set; }
         public string Text { get; set; }
         public DateTime DateFrom { get; set; }
         public DateTime DateTo { get; set; }
+
         public ActionAndBenefitMessage() { }
         public ActionAndBenefitMessage(string pharmacyName, string text, DateTime dateFrom, DateTime dateTo )
         {
