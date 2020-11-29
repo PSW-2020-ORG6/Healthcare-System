e4bf9caf35ab7b03c515f09735ac233b4b64a78f
stefanwert <58173887+stefanwert@users.noreply.github.com>
2020-11-29 15:14:31 +0100

    [GEA] Search for medicine

---
+++ WebApplication/Migrations/20201128185654_num1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication.Migrations
+{
+    public partial class num1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Equipments",
+                columns: new[] { "Id", "Name", "RoomId", "SerialNumber" },
+                values: new object[] { "11", "Bed", "101", "11" });
+
+            migrationBuilder.UpdateData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: "0002",
+                column: "SerialNumber",
+                value: "c3e49d18-55f3-4672-bfb6-e3c3de423fb4");
+
+            migrationBuilder.UpdateData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: "0003",
+                column: "SerialNumber",
+                value: "474e9f09-e3c9-490c-930b-00d7caec5c71");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: "11");
+
+            migrationBuilder.UpdateData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: "0002",
+                column: "SerialNumber",
+                value: "ff517cf5-1013-4048-8c5b-c7648918a1a7");
+
+            migrationBuilder.UpdateData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: "0003",
+                column: "SerialNumber",
+                value: "fb3215f4-fa6d-4af0-8047-a063dbcc410e");
+        }
+    }
+}
