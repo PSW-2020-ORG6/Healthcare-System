50657aaddfe0629019d85d099e0b0c989819b4cf
stefanwert <58173887+stefanwert@users.noreply.github.com>
2020-11-29 16:42:23 +0100

    [GEA] Search of medicine changes2

--- project-generated-code-backend/Backend/Model/Hospital/Floor.cs
+++ project-generated-code-backend/Backend/Model/Hospital/Floor.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 namespace health_clinic_class_diagram.Backend.Model.Hospital
 {
     public class Floor : Entity
-    {
+    {   
         public string Id { get; }
         public string Name { get; set; }
         public List<Room> Rooms { get; set; }
