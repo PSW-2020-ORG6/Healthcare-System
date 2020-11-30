61a345f5fac1c971db06265906962d29c271b18e
milica-okiljevic <58478503+milica-okiljevic@users.noreply.github.com>
2020-11-30 23:33:12 +0100

    [#IA]Receiving messages from pharmacy tests Correction-01

--- IntegrationAdaptersTests/ActionsAndBenefitsTests.cs
+++ IntegrationAdaptersTests/ActionsAndBenefitsTests.cs
@@ -13,12 +13,13 @@ namespace IntegrationAdaptersTests
     public class ActionsAndBenefitsTests
     {
         public Mock<IActionsAndBenefitsRepository> mock;
+        public Guid guid = new Guid("0e89a9a7-2722-45bb-a3de-7832131dcfe3");
+        public Guid guid2 = new Guid("a69b4d8e-55ce-4cdf-bfa6-7462e085a909");
 
         [Fact]
         public void Find_existing_published_message()
         {
             mock = new Mock<IActionsAndBenefitsRepository>();
-            Guid guid = new Guid("0e65a9a7-2722-45bb-a3de-7832131dcfe3");
             mock.Setup(expression: t => t.GetActionAndBenefitMessegeByID(It.IsAny<Guid>())).Returns( new ActionAndBenefitMessage { ActionID = guid, PharmacyName = "Jankovic", Text = "50% off brufen", DateFrom = new DateTime(2020,5,21) ,DateTo = new DateTime(2020,6,21)} );
   
             ActionsAndBenefitsService service = new ActionsAndBenefitsService(mock.Object);
@@ -31,8 +32,6 @@ namespace IntegrationAdaptersTests
         public void Find_not_existing_published_message()
         {
             mock = new Mock<IActionsAndBenefitsRepository>();
-            Guid guid = new Guid("0e89a9a7-2722-45bb-a3de-7832131dcfe3");
-            Guid guid2 = new Guid("a69b4d8e-55ce-4cdf-bfa6-7462e085a909");
             mock.Setup(expression: t => t.GetAllPublishedActionsAndBenefitsMessages()).Returns(new List<ActionAndBenefitMessage>{
                 new ActionAndBenefitMessage { ActionID = guid, PharmacyName = "Jankovic", Text = "50% off brufen", DateFrom = new DateTime(2020, 5, 21), DateTo = new DateTime(2020, 6, 21) }});
 
@@ -66,8 +65,6 @@ namespace IntegrationAdaptersTests
         public void Find_all_actions_and_benefits()
         {
             mock = new Mock<IActionsAndBenefitsRepository>();
-            Guid guid = new Guid("0e89a9a7-2722-45bb-a3de-7832131dcfe3");
-            Guid guid2 = new Guid("a69b4d8e-55ce-4cdf-bfa6-7462e085a909");
             mock.Setup(expression: t => t.GetAllPublishedActionsAndBenefitsMessages()).Returns(new List<ActionAndBenefitMessage>{
                 new ActionAndBenefitMessage { ActionID = guid, PharmacyName = "Jankovic", Text = "50% off brufen", DateFrom = new DateTime(2020, 5, 21), DateTo = new DateTime(2020, 6, 21) }
             ,new ActionAndBenefitMessage { ActionID = new Guid("0e90a9a7-2722-45bb-a3de-7832131dcfe3"), PharmacyName = "BENU",Text = "2 vitamins Mg for 1 price", DateFrom= new DateTime(2020, 2, 16), DateTo = new DateTime(2020, 4, 12)} });
