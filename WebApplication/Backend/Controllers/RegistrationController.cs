39853d36cb3764e9c413cf996a3c3013e6302131
Aleksandra Milijevic <aleksandra.milijevic98@uns.ac.rs>
2020-11-30 04:28:21 +0100

    [#WA-2.3] Correction

--- WebApplication/Backend/Controllers/RegistrationController.cs
+++ WebApplication/Backend/Controllers/RegistrationController.cs
@@ -1,13 +1,6 @@
 ﻿using Backend.Dto;
 using Microsoft.AspNetCore.Mvc;
 using Model.Accounts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
-using WebApplication.Backend.Model;
 using WebApplication.Backend.Services;
 
 namespace WebApplication.Backend.Controllers
