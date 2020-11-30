6c30d39d9b0181124c3269ca83a155d113f75255
Aleksandra Milijevic <aleksandra.milijevic98@uns.ac.rs>
2020-11-30 04:28:21 +0100

    [#WA-2.3] Correction

--- WebApplication/Backend/Util/UtilDbContext.cs
+++ WebApplication/Backend/Util/UtilDbContext.cs
@@ -1,49 +1,36 @@
-﻿using health_clinic_class_diagram.Backend.Model.Survey;
-using health_clinic_class_diagram.Backend.Model.Hospital;
+﻿using health_clinic_class_diagram.Backend.Model.Hospital;
 using Microsoft.EntityFrameworkCore;
 using Model.Accounts;
 using Model.Blog;
 using Model.Hospital;
-using Model.MedicalExam;
 using Model.Util;
 using System;
-using System.Collections.Generic;
 
 namespace WebApplication.Backend.Model
 {
     public class UtilDbContext : DbContext
     {
         public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<Address> Addresses { get; set; }
         public DbSet<Patient> Patients { get; set; }
         public DbSet<Building> Buildings { get; set; }
         public DbSet<Equipment> Equipments { get; set; }
         public DbSet<Floor> Floors { get; set; }
         public DbSet<RoomGEA> Rooms { get; set; }
-        public DbSet<BedGEA> Beds { get; set; }
         public DbSet<MedicineManufacturer> MedicineManufacturers { get; set; }
         public DbSet<MedicineType> MedicineTypes { get; set; }
         public DbSet<Medicine> Medicine { get; set; }
         public DbSet<MedicineGEA> MedicineGEA { get; set; }
 
-        public DbSet<Question> Questions { get; set; }
-        public DbSet<Survey> Surveys { get; set; }
-        public DbSet<Report> Reports { get; set; }
-
-
         public UtilDbContext(DbContextOptions<UtilDbContext> options) : base(options) { }
 
-        // only for testing purposes
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Survey>().HasKey(o => o.SerialNumber );
-            modelBuilder.Entity<Report>().HasKey(o => o.SerialNumber);
-
             modelBuilder.Entity<Feedback>().HasKey(o => o.SerialNumber);
             modelBuilder.Entity<Building>().HasKey(o => o.SerialNumber);
             modelBuilder.Entity<Equipment>().HasKey(o => o.Id);
             modelBuilder.Entity<Floor>().HasKey(o => o.SerialNumber);
             modelBuilder.Entity<RoomGEA>().HasKey(o => o.SerialNumber);
-            modelBuilder.Entity<BedGEA>().HasKey(o => o.SerialNumber);
             modelBuilder.Entity<MedicineManufacturer>().HasKey(o => o.SerialNumber);
             modelBuilder.Entity<MedicineType>().HasKey(o => o.SerialNumber);
             modelBuilder.Entity<Medicine>().HasKey(o => o.SerialNumber);
@@ -54,117 +41,30 @@ namespace WebApplication.Backend.Model
                  new Patient { Name = "Jelena", Surname = "Tanjic", Id = "0002", DateOfBirth = new DateTime(2017, 1, 18), Contact = "kontakt", Password = "sifra", Address = new Address("neka adresa"), ParentName = "otac", Gender = "Zensko", Email = "email", Guest = true },
                  new Patient { Name = "Sara", Surname = "Milic", Id = "0003", DateOfBirth = new DateTime(2018, 1, 18), Contact = "kontaktMica", Password = "sifraMica", Address = new Address("neka adresaMica"), ParentName = "mama", Gender = "Zensko", Email = "emailMica", Guest = true }
             );
-            modelBuilder.Entity<Question>().HasKey(o => o.Id);
-            modelBuilder.Entity<Question>().HasData(
-                new Question { id = 1,QuestionText= "The doctor is welcoming and gentle?"},
-                new Question { id = 2, QuestionText = "The doctor answered all of your questions in an understandable manner?" },
-                new Question { id = 3, QuestionText = "The doctor takes care of you in a professional manner?" },
-                new Question { id = 4, QuestionText = "Would you have the procedure done again by this doctor?" },
-                new Question { id = 5, QuestionText = "The personal manner(courtosy,respect,sensitivity,friendliness) of the nurses and other support staff?" },
-                new Question { id = 6, QuestionText = "The nursees answered all of your questions in an understandable manner?" },
-                new Question { id = 7, QuestionText = "Orientation given to warn setup" },
-                new Question { id = 8, QuestionText = "The nurse gave you good discharge instructions" },
-                new Question { id = 9, QuestionText = "The nurse was concern for you?" },
-                new Question { id = 10, QuestionText = "The comfort and cleanliness of the facility" },
-                new Question { id = 11, QuestionText = "Comfort level within the procedure room?" },
-                new Question { id = 12, QuestionText = "Conditions of the rooms(temperature,comfort,silence)" },
-                new Question { id = 13, QuestionText = "General impression of the ambient atmosphere" },
-                new Question { id = 14, QuestionText = "Do you think the clinic has the necessary equipment" },
-                new Question { id = 15, QuestionText = "Do you think the clinic's farmacy has the necessary drugs?" },
-                new Question { id = 16, QuestionText = "Do you think that the hospital should have more modern equipment than the current one" },
-                new Question { id = 17, QuestionText = "Did you noticed broken or damaged equipment in the hospital" },
-                new Question { id = 18, QuestionText = "The doctor prescribed medications that I could buy at the clinic's pharmacy" },
-                new Question { id = 19, QuestionText = "Did you found it easy to use our website?" },
-                new Question { id = 20, QuestionText = "Did you have found all the necessary information on our website?" },
-                new Question { id = 21, QuestionText = "Overall, are you satisfied with the care you received in this facility?" },
-                new Question { id = 22, QuestionText = "Would you come to this institution again" },
-                new Question { id = 23, QuestionText = ">Would you recommend this facility to your friends and family" }
-           );
-            
-            modelBuilder.Entity<Survey>().HasData(
-                new Survey {
-                    ID = "001",
-                    DoctorName = "Pera Peric",
-                    Question1 = "5",
-                    Question2 = "5",
-                    Question3 = "5",
-                    Question4 = "5",
-                    Question5 = "4",
-                    Question6 = "3",
-                    Question7 = "5",
-                    Question8 = "5",
-                    Question9 = "1",
-                    Question10 = "5",
-                    Question11 = "2",
-                    Question12 = "5",
-                    Question13 = "5",
-                    Question14 = "5",
-                    Question15 = "5",
-                    Question16 = "5",
-                    Question17 = "5",
-                    Question18 = "5",
-                    Question19 = "5",
-                    Question20 = "5",
-                    Question21 = "3",
-                    Question22 = "2",
-                    Question23 = "4"
-                }
-           );
-            modelBuilder.Entity<Survey>().HasData(
-               new Survey
-               {
-                   ID = "005",
-                   DoctorName="Mika Mikic",
-                   Question1 = "5",
-                   Question2 = "5",
-                   Question3 = "5",
-                   Question4 = "5",
-                   Question5 = "4",
-                   Question6 = "3",
-                   Question7 = "5",
-                   Question8 = "5",
-                   Question9 = "1",
-                   Question10 = "5",
-                   Question11 = "2",
-                   Question12 = "5",
-                   Question13 = "5",
-                   Question14 = "5",
-                   Question15 = "5",
-                   Question16 = "5",
-                   Question17 = "5",
-                   Question18 = "5",
-                   Question19 = "5",
-                   Question20 = "5",
-                   Question21 = "3",
-                   Question22 = "2",
-                   Question23 = "4"
-               }
-          );
-
             modelBuilder.Entity<Building>().HasData(
-              new Building { SerialNumber = "10001", Name = "Cardiology", Color = "Orange", Row = 5, Column = 1, Style = "TriangleBuildingButtonStyle" },
-              new Building { SerialNumber = "10002", Name = "Orthopedy", Color = "Red", Row = 5, Column = 3, Style = "UBuildingButtonStyle" }
+              new Building { SerialNumber = "10001", Name = "Cardiology", Color = "Orange", Shape = "Square" },
+              new Building { SerialNumber = "10002", Name = "Orthopedy", Color = "Dark Orange", Shape = "Square" }
             );
             modelBuilder.Entity<Floor>().HasData(
-              new Floor { SerialNumber = "1001", Name = "Floor1", BuildingSerialNumber = "10001" },
-              new Floor { SerialNumber = "1002", Name = "Floor2", BuildingSerialNumber = "10001" },
-              new Floor { SerialNumber = "1003", Name = "Floor1", BuildingSerialNumber = "10002" }
+              new Floor { SerialNumber = "1001", Name = "Floor1", BuildingName = "Cardiology" },
+              new Floor { SerialNumber = "1002", Name = "Floor2", BuildingName = "Cardiology" },
+              new Floor { SerialNumber = "1003", Name = "Floor1", BuildingName = "Orthopedy" }
             );
             modelBuilder.Entity<RoomGEA>().HasData(
-                new RoomGEA { SerialNumber = "101", Name = "Examination room 101", FloorName = "Floor 1", BuildingName = "Cardiology" },
-                new RoomGEA { SerialNumber = "102", Name = "Examination room 102", FloorName = "Floor 1", BuildingName = "Cardiology" },
-                new RoomGEA { SerialNumber = "103", Name = "Store room 103", FloorName = "Floor 1", BuildingName = "Cardiology" },
-                new RoomGEA { SerialNumber = "104", Name = "Examination room 104", FloorName = "Floor 1", BuildingName = "Cardiology" },
-                new RoomGEA { SerialNumber = "105", Name = "Store room 105", FloorName = "Floor 1", BuildingName = "Cardiology" },
-                new RoomGEA { SerialNumber = "106", Name = "Operation room 106", FloorName = "Floor 2", BuildingName = "Cardiology" },
-                new RoomGEA { SerialNumber = "107", Name = "Operation room 107", FloorName = "Floor 2", BuildingName = "Cardiology" },
-                new RoomGEA { SerialNumber = "108", Name = "Store room 108", FloorName = "Floor 2", BuildingName = "Cardiology" },
-                new RoomGEA { SerialNumber = "109", Name = "Examination room 109", FloorName = "Floor 1", BuildingName = "Orthopedy" },
-                new RoomGEA { SerialNumber = "110", Name = "Operation room 110", FloorName = "Floor 1", BuildingName = "Orthopedy" },
-                new RoomGEA { SerialNumber = "111", Name = "Examination room 111", FloorName = "Floor 1", BuildingName = "Orthopedy" },
-                new RoomGEA { SerialNumber = "112", Name = "Store room 112", FloorName = "Floor 1", BuildingName = "Orthopedy" },
-                new RoomGEA { SerialNumber = "113", Name = "Examination room 113", FloorName = "Floor 1", BuildingName = "Orthopedy" },
-                new RoomGEA { SerialNumber = "114", Name = "Examination room 114", FloorName = "Floor 1", BuildingName = "Orthopedy" }
+                new RoomGEA { SerialNumber = "101", Name = "Examination room", FloorName = "Floor 1", BuildingName = "Cardiology" },
+                new RoomGEA { SerialNumber = "102", Name = "Examination room", FloorName = "Floor 1", BuildingName = "Cardiology" },
+                new RoomGEA { SerialNumber = "103", Name = "Store room", FloorName = "Floor 1", BuildingName = "Cardiology" },
+                new RoomGEA { SerialNumber = "104", Name = "Examination room", FloorName = "Floor 1", BuildingName = "Cardiology" },
+                new RoomGEA { SerialNumber = "105", Name = "Store room", FloorName = "Floor 1", BuildingName = "Cardiology" },
+                new RoomGEA { SerialNumber = "106", Name = "Operation room", FloorName = "Floor 2", BuildingName = "Cardiology" },
+                new RoomGEA { SerialNumber = "107", Name = "Operation room", FloorName = "Floor 2", BuildingName = "Cardiology" },
+                new RoomGEA { SerialNumber = "108", Name = "Store room", FloorName = "Floor 2", BuildingName = "Cardiology" },
+                new RoomGEA { SerialNumber = "109", Name = "Examination room", FloorName = "Floor 1", BuildingName = "Orthopedy" },
+                new RoomGEA { SerialNumber = "110", Name = "Operation room", FloorName = "Floor 1", BuildingName = "Orthopedy" },
+                new RoomGEA { SerialNumber = "111", Name = "Examination room", FloorName = "Floor 1", BuildingName = "Orthopedy" },
+                new RoomGEA { SerialNumber = "112", Name = "Store room", FloorName = "Floor 1", BuildingName = "Orthopedy" },
+                new RoomGEA { SerialNumber = "113", Name = "Examination room", FloorName = "Floor 1", BuildingName = "Orthopedy" },
+                new RoomGEA { SerialNumber = "114", Name = "Examination room", FloorName = "Floor 1", BuildingName = "Orthopedy" }
             );
             modelBuilder.Entity<Equipment>().HasData(
                 new Equipment { SerialNumber = "73", RoomId = "101", Name = "Bed", Id = "11" },
@@ -180,8 +80,8 @@ namespace WebApplication.Backend.Model
 
                 );
             modelBuilder.Entity<MedicineManufacturer>().HasData(
-                new MedicineManufacturer { SerialNumber = "1", Name = "manufacturer1"  },
-                new MedicineManufacturer { SerialNumber = "2", Name = "manufacturer2"  }
+                new MedicineManufacturer { Name = "manufacturer1", SerialNumber = "1" },
+                new MedicineManufacturer { Name = "manufacturer2", SerialNumber = "2" }
                 );
             modelBuilder.Entity<MedicineType>().HasData(
                     new MedicineType { SerialNumber = "11", Type = "Antibiotic" },
@@ -191,35 +91,8 @@ namespace WebApplication.Backend.Model
                    new MedicineGEA { SerialNumber = "21", CopyrightName = "Brufen", GenericName = "Brufen", MedicineManufacturerId = "1", MedicineTypeId = "11" },
                    new MedicineGEA { SerialNumber = "22", CopyrightName = "Probiotic", GenericName = "Probiotic", MedicineManufacturerId = "2", MedicineTypeId = "12" }
                 );
-            modelBuilder.Entity<BedGEA>().HasData(
-                new BedGEA
-                {
-                    SerialNumber = "1", 
-                    BuildingSerialNumber = "10001", 
-                    FloorSerialNumber = "1001", 
-                    RoomSerialNumber = "101",
-                    Name = "Bed 1", 
-                    PatientID = "0002"
-                },
-                new BedGEA
-                {
-                    SerialNumber = "2",
-                    BuildingSerialNumber = "10001",
-                    FloorSerialNumber = "1001",
-                    RoomSerialNumber = "101",
-                    Name = "Bed 2",
-                    PatientID = "0003"
-                },
-                new BedGEA
-                {
-                    SerialNumber = "3",
-                    BuildingSerialNumber = "10001",
-                    FloorSerialNumber = "1001",
-                    RoomSerialNumber = "102",
-                    Name = "Bed 3",
-                    PatientID = null
-                }
-            );
+            modelBuilder.Entity<Patient>().HasKey(o=>o.Id);
+            modelBuilder.Entity<Address>().HasKey(o => o.SerialNumber);
         }
     }
 }
