356a290c19f403e713df7ccc0131893818cc9994
Milica Siriski <milica.siriski998@gmail.com>
2020-12-04 19:11:05 +0100

    [#DevOps1] Shared db works with medicine entities.

--- project-generated-code-backend/Backend/Repository/DatabaseSql/HealthCareSystemDbContext.cs
+++ project-generated-code-backend/Backend/Repository/DatabaseSql/HealthCareSystemDbContext.cs
@@ -19,18 +19,18 @@ namespace HealthClinicBackend.Backend.Repository.DatabaseSql
         // public DbSet<Physician> Physicians { get; set; }
         // public DbSet<Secretary> Secretaries { get; set; }
 
-        public DbSet<Equipment> Equipments { get; set; }
-        public DbSet<Bed> Beds { get; set; }
-        public DbSet<RoomType> RoomTypes { get; set; }
-        public DbSet<Room> Rooms { get; set; }
-        public DbSet<Floor> Floors { get; set; }
-        public DbSet<Building> Buildings { get; set; }
+        // public DbSet<Equipment> Equipments { get; set; }
+        // public DbSet<Bed> Beds { get; set; }
+        // public DbSet<RoomType> RoomTypes { get; set; }
+        // public DbSet<Room> Rooms { get; set; }
+        // public DbSet<Floor> Floors { get; set; }
+        // public DbSet<Building> Buildings { get; set; }
 
-        // public DbSet<MedicineManufacturer> MedicineManufacturers { get; set; }
-        // public DbSet<MedicineType> MedicineTypes { get; set; }
-        // public DbSet<Medicine> Medicines { get; set; }
-        // public DbSet<Rejection> Rejections { get; set; }
-        //
+        public DbSet<MedicineManufacturer> MedicineManufacturers { get; set; }
+        public DbSet<MedicineType> MedicineTypes { get; set; }
+        public DbSet<Medicine> Medicines { get; set; }
+        public DbSet<Rejection> Rejections { get; set; }
+
         // public DbSet<DiagnosticType> DiagnosticTypes { get; set; }
         // public DbSet<DiagnosticReferral> DiagnosticReferrals { get; set; }
         // public DbSet<FollowUp> FollowUps { get; set; }
@@ -55,27 +55,40 @@ namespace HealthClinicBackend.Backend.Repository.DatabaseSql
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Building>()
-                .HasMany(b => b.Floors) // Building has many references to Floors
-                .WithOne(f => f.Building) // Floors have one reference to Building
-                .OnDelete(DeleteBehavior.Cascade); // On deleting one building all the referenced floors are deleted
+            modelBuilder.Entity<Medicine>()
+                .HasOne(m => m.MedicineManufacturer) // Medicine has one Medicine Manufacturer
+                .WithMany(); // Medicine Manufacturer has many Medicine but doesn't reference them
 
-            modelBuilder.Entity<Floor>()
-                .HasMany(f => f.Rooms) // Floor has many Rooms
-                .WithOne(r => r.Floor) // Room has one Floor
-                .OnDelete(DeleteBehavior.Cascade); // When Floor is deleted, so are all the referenced Rooms
+            modelBuilder.Entity<Medicine>()
+                .HasOne(m => m.MedicineType)
+                .WithMany();
 
-            modelBuilder.Entity<Room>()
-                .HasMany(r => r.Equipment) // Room has many Equipments
-                .WithOne(); // Equipment 'has' one Room but doesn't reference it
+            modelBuilder.Entity<Rejection>()
+                .HasOne(r => r.Medicine)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
 
-            modelBuilder.Entity<Room>()
-                .HasMany(r => r.Beds) // Room has many Beds
-                .WithOne(); // Bed 'has' one Room but doesn't reference it
-
-            modelBuilder.Entity<Room>()
-                .HasOne(r => r.RoomType) // Room has RoomType
-                .WithMany(); // RoomType can have many Rooms but doesn't reference them
+            // modelBuilder.Entity<Building>()
+            //     .HasMany(b => b.Floors) // Building has many references to Floors
+            //     .WithOne(f => f.Building) // Floors have one reference to Building
+            //     .OnDelete(DeleteBehavior.Cascade); // On deleting one building all the referenced floors are deleted
+            //
+            // modelBuilder.Entity<Floor>()
+            //     .HasMany(f => f.Rooms) // Floor has many Rooms
+            //     .WithOne(r => r.Floor) // Room has one Floor
+            //     .OnDelete(DeleteBehavior.Cascade); // When Floor is deleted, so are all the referenced Rooms
+            //
+            // modelBuilder.Entity<Room>()
+            //     .HasMany(r => r.Equipment) // Room has many Equipments
+            //     .WithOne(); // Equipment 'has' one Room but doesn't reference it
+            //
+            // modelBuilder.Entity<Room>()
+            //     .HasMany(r => r.Beds) // Room has many Beds
+            //     .WithOne(); // Bed 'has' one Room but doesn't reference it
+            //
+            // modelBuilder.Entity<Room>()
+            //     .HasOne(r => r.RoomType) // Room has RoomType
+            //     .WithMany(); // RoomType can have many Rooms but doesn't reference them
         }
     }
 }
--- project-generated-code-backend/Backend/Repository/DatabaseSql/RoomDatabaseSql.cs
+++ project-generated-code-backend/Backend/Repository/DatabaseSql/RoomDatabaseSql.cs
@@ -8,33 +8,33 @@ namespace HealthClinicBackend.Backend.Repository.DatabaseSql
 {
     public class RoomDatabaseSql: GenericDatabaseSql<Room>, IRoomRepository
     {
-        public override List<Room> GetAll()
-        {
-            return dbContext.Rooms.ToList();
-        }
-
-        public override void Save(Room newEntity)
-        {
-            dbContext.Rooms.Add(newEntity);
-            dbContext.SaveChanges();
-        }
-
-        public override void Update(Room updateEntity)
-        {
-            dbContext.Rooms.Update(updateEntity);
-            dbContext.SaveChanges();
-        }
-
-        public void Delete(Room entity)
-        {
-            dbContext.Rooms.Remove(entity);
-            dbContext.SaveChanges();
-        }
-
-        public override Room GetById(string id)
-        {
-            return dbContext.Rooms.Find(id);
-        }
+        // public override List<Room> GetAll()
+        // {
+        //     return dbContext.Rooms.ToList();
+        // }
+        //
+        // public override void Save(Room newEntity)
+        // {
+        //     dbContext.Rooms.Add(newEntity);
+        //     dbContext.SaveChanges();
+        // }
+        //
+        // public override void Update(Room updateEntity)
+        // {
+        //     dbContext.Rooms.Update(updateEntity);
+        //     dbContext.SaveChanges();
+        // }
+        //
+        // public void Delete(Room entity)
+        // {
+        //     dbContext.Rooms.Remove(entity);
+        //     dbContext.SaveChanges();
+        // }
+        //
+        // public override Room GetById(string id)
+        // {
+        //     return dbContext.Rooms.Find(id);
+        // }
 
         public List<Room> GetRoomsByProcedureType(ProcedureType procedureType)
         {
