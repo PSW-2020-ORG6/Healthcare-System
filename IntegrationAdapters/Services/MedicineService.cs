0b45f95a85f0a36badd57bd2694035267bde5c31
ljiljanaknezevic <58477796+ljiljanaknezevic@users.noreply.github.com>
2020-12-09 15:16:14 +0100

    prescribe medication front

---
+++ IntegrationAdapters/Services/MedicineService.cs
@@ -0,0 +1,39 @@
+﻿using IntegrationAdapters.Models;
+using IntegrationAdapters.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationAdapters.Services
+{
+    public class MedicineService
+    {
+        private IMedicineRepository medicineRepository;
+
+        public MedicineService()
+        {
+            this.medicineRepository = new MedicineRepository();
+        }
+      /*  public MedicineService(IMedicineRepository medicineRepository)
+        {
+            this.medicineRepository = medicineRepository;
+        }*/
+        public List<Medicine> GetAll()
+        {
+            return medicineRepository.GetAll();
+        }
+        public Medicine GetMedicineByID(String ID)
+        {
+            return medicineRepository.GetByID(ID);
+        }
+        public Medicine GetMedicineByName(String Name)
+        {
+            return medicineRepository.GetByName(Name);
+        }
+        public void AddMedicine()
+        {
+            medicineRepository.AddMedicineRepository();
+        }
+    }
+}
---
+++ IntegrationAdapters/wwwroot/js/PrescribeMedicine.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+	$("#btnGetMedicineSpecification").click(function () {
+		var MedicineName = $("#txtMedicine").val();
+		$.get({
+			url: '../medicine/getMedicineSpecification/' + MedicineName,
+			contentType: 'application/json',
+			success: function (MedicineName) {
+				console.log(MedicineName);
+				if (MedicineName != "")
+					alert('Specification found!');
+				else {
+					$("#btnAskPharmaciesForSpecification").attr("disabled", false);
+					$("#btnGetMedicineSpecification").attr("disabled", true);
+					alert('Specification not found!Ask pharmacy!');
+				}
+			},
+			error: function (message) {
+				alert("Failed")
+			}
+		});
+	});
+});
