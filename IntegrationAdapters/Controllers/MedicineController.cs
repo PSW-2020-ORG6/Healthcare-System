3afb6fea4d44e08112fad16105fb563f243b1c1f
dragana-todorovic <dragana.todorovic@uns.ac.rs>
2020-12-10 13:49:15 +0100

    [#IA] Get specification about medication from pharmacy

--- IntegrationAdapters/Controllers/MedicineController.cs
+++ IntegrationAdapters/Controllers/MedicineController.cs
@@ -1,16 +1,21 @@
 ﻿using IntegrationAdapters.Models;
+using IntegrationAdapters.Models.DTO;
 using IntegrationAdapters.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
+
 
 namespace IntegrationAdapters.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class MedicineController
+    public class MedicineController : ControllerBase
     {
         private readonly MedicineService medicineService;
 
@@ -30,6 +35,37 @@ namespace IntegrationAdapters.Controllers
             {
                 return "";
             }
+        }
+
+        [HttpPost("prescribeMedicine")]
+        public IActionResult PrescribeMedicine(PrescriptionDTO prescription)
+        {
+            var fileName = "Prescription " + DateTime.Now.ToString("yyyy-MM-dd") + ".txt";
+            medicineService.GeneratePrescription(prescription, fileName);
+            return Ok();
+        }
+
+        [HttpGet("getSpecification/{MedicineName}")]
+        public IActionResult GetSpecification(string MedicineName)
+        {
+            var endPoint = "http://localhost:8082/myapp/medication/getSpecification/" + MedicineName;
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(endPoint);
+            HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
+            Stream receiveStream = webResponse.GetResponseStream();
+            Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+            StreamReader readStream = new StreamReader(receiveStream, encode);
+            string text = readStream.ReadToEnd();
+            if (text.Length != 0)
+            {
+                medicineService.GenerateSpecification(text, "Specification for " + MedicineName + ".txt");
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+            
         }
     }
 }
--- IntegrationAdapters/wwwroot/js/PrescribeMedicine.js
+++ IntegrationAdapters/wwwroot/js/PrescribeMedicine.js
@@ -19,4 +19,42 @@
 			}
 		});
 	});
+
+	$("#btnAskPharmaciesForSpecification").click(function () {
+		var MedicineName = $("#txtMedicine").val();
+		$.get({
+			url: '../medicine/getSpecification/' + MedicineName,
+			contentType: 'application/json',
+			success: function () {
+				alert('Success found specification in pharmacy');
+	
+			},
+			error: function (message) {
+				$("#btnAskPharmaciesForSpecification").attr("disabled", true);
+				$("#btnGetMedicineSpecification").attr("disabled", false);
+				alert('Specification not found in phmarmacy');
+			}
+		});
+	});
+
+	$("#btnPrescribe").click(function () {
+		var PatientName = $("#txtPatientName").val();
+		var PatientSurName = $("#txtPatientSurname").val();
+		var Medicine = $("#txtMedicine").val();
+		var Quantity = parseInt($("#txtQuantity").val());
+		var PharmacyName = $("#txtPharmacyName").val();
+		var Note = $("#txtNote").val();
+		$.post({
+			url: '../medicine/prescribeMedicine',
+			data: JSON.stringify({ PatientName: PatientName, PatientSurName: PatientSurName, Medicine: Medicine, Quantity: Quantity, PharmacyName: PharmacyName, Note: Note }),
+			contentType: 'application/json',
+			success: function (data) {
+				alert("Succes sent prescription");
+				location.href = "../index.html";
+			},
+			error: function (message) {
+				alert("Failed sent")
+			}
+		});
+	});
 });
