07d6c05777b86f025b3cf3d3c42bc796fd83b7a0
milica-okiljevic <58478503+milica-okiljevic@users.noreply.github.com>
2020-12-10 01:04:56 +0100

    Receiving messages from pharmacy using grpc protocol Correction01

--- IntegrationAdaptersTests/ActionsAndBenefitsTests.cs
+++ IntegrationAdaptersTests/ActionsAndBenefitsTests.cs
@@ -13,8 +13,11 @@ namespace IntegrationAdaptersTests
     public class ActionsAndBenefitsTests
     {
         public Mock<IActionsAndBenefitsRepository> mock;
-        public Guid guid = new Guid("0e89a9a7-2722-45bb-a3de-7832131dcfe3");
-        public Guid guid2 = new Guid("a69b4d8e-55ce-4cdf-bfa6-7462e085a909");
+        public static Guid guid = new Guid("0e89a9a7-2722-45bb-a3de-7832131dcfe3");
+        public static Guid guid2 = new Guid("a69b4d8e-55ce-4cdf-bfa6-7462e085a909");
+        public ActionAndBenefitMessage actionAndBenefitMessage1 = new ActionAndBenefitMessage(guid, "Jankovic", "50% off brufen", new DateTime(2020, 5, 21), new DateTime(2020, 6, 21));
+        public ActionAndBenefitMessage actionAndBenefitMessage2 = new ActionAndBenefitMessage(new Guid("0e90a9a7-2722-45bb-a3de-7832131dcfe3"), "BENU", "2 vitamins Mg for 1 price",  new DateTime(2020, 2, 16), new DateTime(2020, 4, 12));
+        public ActionAndBenefitMessage actionAndBenefitMessage3 = new ActionAndBenefitMessage(new Guid("0e65a9a7-2722-45bb-a3de-7832131dcfe3"), "Jankovic", "aspirin 50% off", new DateTime(2020, 11, 16), new DateTime(2020, 4, 12));
 
         [Fact]
         public void Find_existing_published_message()
@@ -32,9 +35,7 @@ namespace IntegrationAdaptersTests
         public void Find_not_existing_published_message()
         {
             mock = new Mock<IActionsAndBenefitsRepository>();
-            mock.Setup(expression: t => t.GetAllPublishedActionsAndBenefitsMessages()).Returns(new List<ActionAndBenefitMessage>{
-                new ActionAndBenefitMessage { ActionID = guid, PharmacyName = "Jankovic", Text = "50% off brufen", DateFrom = new DateTime(2020, 5, 21), DateTo = new DateTime(2020, 6, 21) }});
-
+            mock.Setup(expression: t => t.GetAllPublishedActionsAndBenefitsMessages()).Returns(new List<ActionAndBenefitMessage> { actionAndBenefitMessage1});
             ActionsAndBenefitsService service = new ActionsAndBenefitsService(mock.Object);
             ActionAndBenefitMessage actionAndBenefitMessage = service.GetActionAndBenefitMessegeByID(guid2);
 
@@ -45,8 +46,7 @@ namespace IntegrationAdaptersTests
         public void Is_message_already_published()
         {
             ActionsAndBenefitsService service = new ActionsAndBenefitsService();
-            ActionAndBenefitMessage abmessage = new ActionAndBenefitMessage(new Guid("0e65a9a7-2722-45bb-a3de-7832131dcfe3"), "Jankovic", "aspirin 50% off", new DateTime(2020,11,16), new DateTime(2020,4,12));
-            bool result = service.IsMessageExisting(abmessage);
+            bool result = service.IsMessageExisting(actionAndBenefitMessage3);
 
             Assert.True(result);
         }
@@ -55,22 +55,10 @@ namespace IntegrationAdaptersTests
         public void Is_message_not_already_published()
         {
             ActionsAndBenefitsService service = new ActionsAndBenefitsService();
-            ActionAndBenefitMessage abmessage = new ActionAndBenefitMessage(new Guid("0e90a9a7-2722-45bb-a3de-7832131dcfe3"), "Jankovic", "aspirin 50% off", new DateTime(2020, 11, 16), new DateTime(2020, 4, 12));
+            ActionAndBenefitMessage abmessage = actionAndBenefitMessage2;
             bool result = service.IsMessageExisting(abmessage);
 
             Assert.False(result);
-        }
-
-        [Fact]
-        public void Find_all_actions_and_benefits()
-        {
-            mock = new Mock<IActionsAndBenefitsRepository>();
-            mock.Setup(expression: t => t.GetAllPublishedActionsAndBenefitsMessages()).Returns(new List<ActionAndBenefitMessage>{
-                new ActionAndBenefitMessage { ActionID = guid, PharmacyName = "Jankovic", Text = "50% off brufen", DateFrom = new DateTime(2020, 5, 21), DateTo = new DateTime(2020, 6, 21) }
-            ,new ActionAndBenefitMessage { ActionID = new Guid("0e90a9a7-2722-45bb-a3de-7832131dcfe3"), PharmacyName = "BENU",Text = "2 vitamins Mg for 1 price", DateFrom= new DateTime(2020, 2, 16), DateTo = new DateTime(2020, 4, 12)} });
-
-            ActionsAndBenefitsService service = new ActionsAndBenefitsService(mock.Object);
-            service.GetAllPublishedActionsAndBenefitsMessages().Count.CompareTo(2);
         }
     }
 }
