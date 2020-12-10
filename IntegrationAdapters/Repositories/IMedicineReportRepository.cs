a923e1db82c0364ae6d7c28168e4b83987d9842f
Milica Siriski <milica.siriski998@gmail.com>
2020-12-10 14:07:22 +0100

    [#DevOps2] Modified integration adapter project repositories so they work with central database.

--- IntegrationAdapters/Repositories/IMedicineReportRepository.cs
+++ IntegrationAdapters/Repositories/IMedicineReportRepository.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HealthClinicBackend.Backend.Model.PharmacySupport;
+using HealthClinicBackend.Backend.Repository.Generic;
 
 namespace IntegrationAdapters.Repositories
 {
-    public interface IMedicineReportRepository
+    public interface IMedicineReportRepository : IGenericRepository<MedicineReport>
     {
         public List<MedicineReport> GetAll();
         void AddPrescription();
