a79f2dfe6e1692f40024c32f0c25938272eb3e4b
Ana Atanackovic <anaatanackovic98@gmail.com>
2020-12-13 22:02:45 +0100

    [#GEA] search window modified for advanced search

--- GraphicEditor/ViewModel/CardiologyFirstFloorMapUserControlViewModel.cs
+++ GraphicEditor/ViewModel/CardiologyFirstFloorMapUserControlViewModel.cs
@@ -1,23 +1,58 @@
 ﻿using GraphicEditor.HelpClasses;
 using GraphicEditor.View.Windows;
+using HealthClinicBackend.Backend.Model.Hospital;
 using HealthClinicBackend.Backend.Model.Util;
+using HealthClinicBackend.Backend.Repository.DatabaseSql;
+using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace GraphicEditor.ViewModel
 {
     public class CardiologyFirstFloorMapUserControlViewModel : BindableBase
     {
-        public MyICommand<string> ShowRoomCommand { get; private set; }
+        public MyICommand<Room> ShowRoomCommand { get; private set; }
+
+        RoomDatabaseSql roomRepository = new RoomDatabaseSql();
+        BedDatabaseSql bedRepository = new BedDatabaseSql();
+        EquipmentDatabaseSql equipmentRepository = new EquipmentDatabaseSql();
+        RoomTypeDatabaseSql roomTypeRepository = new RoomTypeDatabaseSql();
+
+        public ResourceDictionary ResourceDictionary = new ResourceDictionary();
 
         public CardiologyFirstFloorMapUserControlViewModel()
         {
-            ShowRoomCommand = new MyICommand<string>(ShowRoom);
+            ShowRoomCommand = new MyICommand<Room>(ShowRoom);
         }
 
-        private void ShowRoom(string roomName)
+        public void InitialGridRender()
+        {
+            ResourceDictionary.Source = new Uri("/GraphicEditor;component/Resources/Styles/ButtonStyles.xaml", UriKind.RelativeOrAbsolute);
+            foreach (Room room in roomRepository.GetAll())
+            {
+                Button button = new Button();
+                button.Style = (Style)ResourceDictionary[room.Style];
+                button.Name = room.Name;
+                Grid.SetColumnSpan(button, room.ColumnSpan);
+                Grid.SetRowSpan(button, room.RowSpan);
+                Grid.SetColumn(button, room.Column);
+                Grid.SetRow(button, room.Row);
+                button.Command = ShowRoomCommand;
+                button.CommandParameter = room;
+                room.Beds = bedRepository.GetByRoomSerialNumber(room.SerialNumber);
+                room.RoomType = roomTypeRepository.GetBySerialNumber(room.RoomTypeSerialNumber);
+                room.Equipment = equipmentRepository.GetByRoomSerialNumber(room.SerialNumber);
+
+                //HospitalMapGrid.Children.Add(button);
+                //HospitalMapGrid.UpdateLayout();
+            }
+        }
+
+        private void ShowRoom(Room room)
         {
             if (MainWindow.TypeOfUser != TypeOfUser.Patient)
             {
-                new RoomInformation(roomName).Show();
+                new RoomInformation(room).Show();
             }
             else
             {
--- project-generated-code-backend/Backend/Repository/Generic/IBedRepository.cs
+++ project-generated-code-backend/Backend/Repository/Generic/IBedRepository.cs
@@ -1,9 +1,10 @@
 ﻿using HealthClinicBackend.Backend.Model.Hospital;
+using System.Collections.Generic;
 
 namespace HealthClinicBackend.Backend.Repository.Generic
 {
     public interface IBedRepository : IGenericRepository<Bed>
     {
-
+        List<Bed> GetByRoomSerialNumber(string roomSerialNumber);
     }
 }
