0d749e2a635971eda35da6783487f1871f7885d4
Hadzija <hadzijasale@gmail.com>
2020-12-13 18:34:46 +0100

    [#GEA]Added missing files

---
+++ IntegrationAdapters/IAHealthCareSystemDbContext.cs
@@ -0,0 +1,26 @@
+﻿using IntegrationAdapters.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationAdapters
+{
+    public class IAHealthCareSystemDbContext : DbContext
+    {
+        public DbSet<Api> Apis { get; set; }
+        public DbSet<ActionAndBenefitMessage> ActionAndBenefitMessage { get; set; }
+        public DbSet<MedicineReport> Reports { get; set; }
+        public DbSet<Medicine> Medicine { get; set; }
+        public DbSet<MedicineSpecification> MedicineSpecification { get; set; }
+
+        public IAHealthCareSystemDbContext(DbContextOptions<IAHealthCareSystemDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ IntegrationAdapters/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IntegrationAdapters.Adapters;
+using IntegrationAdapters.Controllers;
+using IntegrationAdapters.gRPCProtocol;
+using IntegrationAdapters.Models;
+using IntegrationAdapters.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace IntegrationAdapters
+{
+    public class Program
+    {
+        public static List<ActionAndBenefitMessage> Messages = new List<ActionAndBenefitMessage>();
+        public static string ResponseMessageGrpc;
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<RabbitMQService>();
+                    services.AddHostedService<ClientScheduleService>();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
