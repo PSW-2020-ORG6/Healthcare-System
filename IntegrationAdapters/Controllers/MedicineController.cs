3fd4e0d226b348c1b20efae559fb21810802a9b6
dragana-todorovic <dragana.todorovic@uns.ac.rs>
2020-12-13 17:59:51 +0100

    [#IA] Check if pharmacy has medication using http

--- IntegrationAdapters/Controllers/MedicineController.cs
+++ IntegrationAdapters/Controllers/MedicineController.cs
@@ -55,12 +55,7 @@ namespace IntegrationAdapters.Controllers
         public IActionResult GetSpecification(string medicineName)
         {
             var endPoint = "http://localhost:8082/myapp/medication/getSpecification/" + medicineName;
-            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(endPoint);
-            HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
-            Stream receiveStream = webResponse.GetResponseStream();
-            Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
-            StreamReader readStream = new StreamReader(receiveStream, encode);
-            string text = readStream.ReadToEnd();
+            string text = GenerateResponse(endPoint);
             if (IsResponseValid(text))
             {
                 medicineService.GenerateSpecificationFromPharmacy(text, medicineName);
@@ -74,10 +69,37 @@ namespace IntegrationAdapters.Controllers
 
         }
 
+        [HttpGet("getPharmacies/{medicineName}/{quantity}")]
+        public string GetPharmacies(string medicineName, int quantity)
+        {
+            var endPoint = "http://localhost:8082/myapp/medication/getPharmacies/" + medicineName + "/" + quantity;
+            string text = GenerateResponse(endPoint);
+            
+            if (IsResponseValid(text))
+            {
+                return text;
+            }
+            else
+            {
+                return "Pharmacy not found";
+            }
+        }
+
         private bool IsResponseValid(string text)
         {
             return text.Length != 0;
 
+        }
+
+        private string GenerateResponse(string endPoint)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(endPoint);
+            HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
+            Stream receiveStream = webResponse.GetResponseStream();
+            Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+            StreamReader readStream = new StreamReader(receiveStream, encode);
+            string text = readStream.ReadToEnd();
+            return text;
         }
 
         [HttpPost("sendMessageGrpc")]
@@ -92,5 +114,7 @@ namespace IntegrationAdapters.Controllers
         {
             return Ok(Program.ResponseMessageGrpc);
         }
+
+        
     }
 }
