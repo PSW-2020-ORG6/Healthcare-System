4cdd7ef5dd4146f4b7f0ec311206ac3bcae9fb3e
Aleksandra Milijevic <aleksandra.milijevic98@uns.ac.rs>
2020-12-19 22:36:36 +0100

    Corrections

--- WebApplication/Backend/Repositorys/PrescriptionRepository.cs
+++ WebApplication/Backend/Repositorys/PrescriptionRepository.cs
@@ -32,6 +32,7 @@ namespace WebApplication.Backend.Repositorys
         ///</returns>
         private List<Prescription> GetPrescriptions(string sqlDml)
         {
+            connection.Close();
             connection.Open();
             MySqlCommand sqlCommand = new MySqlCommand(sqlDml, connection);
             MySqlDataReader sqlReader = sqlCommand.ExecuteReader();
@@ -47,7 +48,7 @@ namespace WebApplication.Backend.Repositorys
             connection.Close();
             foreach (Prescription prescription in prescriptions)
             {
-                prescription.MedicineDosage = GetMedicineDosage("Select medicinedosages.SerialNumber,medicinedosages.Note,medicinedosages.Amount,medicines.SerialNumber,medicines.GenericName,medicines.CopyrightName,medicinetypes.SerialNumber,medicinetypes.Type  from medicinedosages,medicines,medicinetypes where medicinedosages.PrescriptionSerialNumber like '" + prescription.SerialNumber + "' and medicinedosages.MedicineSerialNumber like medicines.SerialNumber and medicines.MedicineTypeSerialNumber like medicinetypes.SerialNumber");
+                prescription.MedicineDosage = GetMedicineDosage("Select medicinedosage.SerialNumber,medicinedosage.Note,medicinedosage.Amount,medicine.SerialNumber,medicine.GenericName,medicine.CopyrightName,medicinetype.SerialNumber,medicinetype.Type  from medicinedosage,medicine,medicinetype where medicinedosage.PrescriptionSerialNumber like '" + prescription.SerialNumber + "' and medicinedosage.MedicineSerialNumber like medicine.SerialNumber and medicine.MedicineTypeSerialNumber like medicinetype.SerialNumber");
             }
             return prescriptions;
         }
