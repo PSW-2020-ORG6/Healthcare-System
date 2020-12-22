67d30ed43f1e41aa4ec07291a70c2ff1528da47a
Milica Siriski <milica.siriski998@gmail.com>
2020-12-22 16:19:37 +0100

    Retrieved overwritten dependency injection for web app.

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -1,4 +1,3 @@
-using HealthClinicBackend.Backend.Repository;
 using HealthClinicBackend.Backend.Repository.DatabaseSql;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -13,9 +12,11 @@ using HealthClinicBackend.Backend.Repository.Generic;
 using WebApplication.Backend.Controllers;
 using WebApplication.Backend.Model;
 using WebApplication.Backend.Repositorys;
-using WebApplication.Backend.Repositorys.Interfaces;
 using WebApplication.Backend.Services;
 using IPatientRepository = HealthClinicBackend.Backend.Repository.Generic.IPatientRepository;
+using IPrescriptionRepository = HealthClinicBackend.Backend.Repository.Generic.IPrescriptionRepository;
+using IReportRepository = HealthClinicBackend.Backend.Repository.Generic.IReportRepository;
+using ISurveyRepository = HealthClinicBackend.Backend.Repository.Generic.ISurveyRepository;
 
 namespace WebApplication
 {
@@ -33,11 +34,8 @@ namespace WebApplication
         {
             services.AddControllers();
 
-        
             services.AddDbContext<HealthCareSystemDbContext>(options =>
             {
-                // options.UseNpgsql(
-                //     dataBaseConnectionSettings.ConnectionString);
                 options.UseNpgsql(
                     CreateConnectionStringFromEnvironment(),
                     x => x.MigrationsAssembly("Backend").EnableRetryOnFailure(5,
@@ -48,11 +46,36 @@ namespace WebApplication
             services.Configure<MailSettings>(Configuration.GetSection("MailSettings"));
             services.AddTransient<IMailService, MailService>();
 
+            // Repository dependency injection
             services.AddScoped<IPhysicianRepository, PhysicianDatabaseSql>();
             services.AddScoped<IPatientRepository, PatientDatabaseSql>();
+            services.AddScoped<IAppointmentRepository, AppointmentDatabaseSql>();
+            services.AddScoped<IFeedbackRepository, FeedbackDatabaseSql>();
+            services.AddScoped<IPatientRepository, PatientDatabaseSql>();
+            services.AddScoped<IPrescriptionRepository, PrescriptionDatabaseSql>();
+            services.AddScoped<IReportRepository, ReportDatabaseSql>();
+            services.AddScoped<ISurveyRepository, SurveyDatabaseSql>();
+
+            // Repository-service hybrids dependency injection
+            // TODO: refactor to a normal service
             services.AddScoped<IRegistrationRepository, RegistrationRepository>();
+
+            // Service dependency injection
             services.AddScoped<IRegistrationService, RegistrationService>();
+            services.AddScoped<AppointmentsService, AppointmentsService>();
+            services.AddScoped<FeedbackService, FeedbackService>();
+            services.AddScoped<PatientService, PatientService>();
+            services.AddScoped<PrescriptionService, PrescriptionService>();
+            services.AddScoped<RegistrationService, RegistrationService>();
+            services.AddScoped<ReportService, ReportService>();
+            services.AddScoped<SurveyService, SurveyService>();
+
+            // Controller dependency injection
             services.AddScoped<RegistrationController, RegistrationController>();
+            services.AddScoped<AppointmentController, AppointmentController>();
+            services.AddScoped<FeedbackController, FeedbackController>();
+            services.AddScoped<SurveyController, SurveyController>();
+            services.AddScoped<UserController, UserController>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,45 +88,18 @@ namespace WebApplication
 
             using (var scope = app.ApplicationServices.CreateScope())
             using (var context = scope.ServiceProvider.GetService<HealthCareSystemDbContext>())
-            //using (var controller = scope.ServiceProvider.GetService<RegistrationController>())
             {
                 Console.WriteLine("Connecting to db");
                 foreach (var medicineManufacturer in context.MedicineManufacturer)
                 {
                     Console.WriteLine(medicineManufacturer.Name);
                 }
-            
+
                 Console.WriteLine("Loading physicians");
                 foreach (var physician in context.Physician)
                 {
                     Console.WriteLine($"{physician.Name} {physician.Surname}");
                 }
-
-                /*if (controller == null)
-                {
-                    Console.WriteLine("Controller is null");
-                }
-                else
-                {
-                    Console.WriteLine("Loading physicians");
-                    foreach (var physician in controller.GetAllGeneralPractitioners())
-                    {
-                        Console.WriteLine($"{physician.Name} {physician.Surname} {physician.Specialization}");
-                    }
-                }*/
-
-                // try
-                // {
-                //     Console.WriteLine("Data seeding started.");
-                //     DataSeeder seeder = new DataSeeder(true);
-                //     seeder.SeedAll(context);
-                //     Console.WriteLine("Data seeding finished.");
-                // } catch(Exception e)
-                // {
-                //     Console.WriteLine("Data seeding failed.");
-                //     Console.WriteLine(e.Message);
-                //     Console.WriteLine(e.StackTrace);
-                // }
             }
 
             app.UseRouting();
@@ -118,9 +114,12 @@ namespace WebApplication
             string server = Environment.GetEnvironmentVariable("DATABASE_HOST") ?? "localhost";
             string port = Environment.GetEnvironmentVariable("DATABASE_PORT") ?? "5432";
             string database = Environment.GetEnvironmentVariable("DATABASE_SCHEMA") ?? "healthcare-system-db";
-            string user = Environment.GetEnvironmentVariable("DATABASE_USERNAME") ?? "user";
-            string password = Environment.GetEnvironmentVariable("DATABASE_PASSWORD") ?? "password";
-            string str = "User ID =postgres;Password=super;Server=localhost;Port=5432;Database=healthcare-system-db;Integrated Security=true;Pooling=true;";
+
+            // Change this if user and password are different
+            string user = Environment.GetEnvironmentVariable("DATABASE_USERNAME") ?? "postgres";
+            string password = Environment.GetEnvironmentVariable("DATABASE_PASSWORD") ?? "super";
+            string str =
+                "User ID =postgres;Password=super;Server=localhost;Port=5432;Database=healthcare-system-db;Integrated Security=true;Pooling=true;";
 
             return $"userid={user};server={server};port={port};database={database};password={password};";
         }
