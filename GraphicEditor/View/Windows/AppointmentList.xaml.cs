2583eb174301d4f0f9082c73a34f10cbdd64761b
stefanwert <58173887+stefanwert@users.noreply.github.com>
2020-12-24 17:29:00 +0100

    [#GEA]Appointment scheduling

--- GraphicEditor/View/Windows/AppointmentList.xaml.cs
+++ GraphicEditor/View/Windows/AppointmentList.xaml.cs
@@ -1,4 +1,5 @@
-﻿using HealthClinicBackend.Backend.Dto;
+﻿using HealthClinicBackend.Backend.Controller.SecretaryControllers;
+using HealthClinicBackend.Backend.Dto;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
@@ -11,6 +12,9 @@ namespace GraphicEditor.View.Windows
 
         List<AppointmentDto> appointmentDtos = new List<AppointmentDto>();
         public event PropertyChangedEventHandler PropertyChanged;
+        private int selectedIndex=-1;
+        public SecretaryScheduleController secretaryScheduleController = new SecretaryScheduleController();
+        private Window parent;
 
         public List<AppointmentDto> AppointmentDtos
         {
@@ -25,11 +29,12 @@ namespace GraphicEditor.View.Windows
         {
             InitializeComponent();
         }
-        public AppointmentList(List<AppointmentDto> listOfAppointments)
+        public AppointmentList(List<AppointmentDto> listOfAppointments, Window parent)
         {
             InitializeComponent();
             this.DataContext = this;
             appointmentDtos = listOfAppointments;
+            this.parent = parent;
         }
 
         protected void OnPropertyChanged([CallerMemberName] string name = null)
@@ -37,10 +42,35 @@ namespace GraphicEditor.View.Windows
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
 
-        private void showEquipmentDetails(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        private void selectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
-            int index = appointmentListBox.SelectedIndex;
-            string serialNumber = appointmentDtos[index].Room.SerialNumber;
+            selectedIndex = appointmentListBox.SelectedIndex;
+            /*string serialNumber = appointmentDtos[index].Room.SerialNumber;
+            EquipmentDetails win = new EquipmentDetails(serialNumber);
+            win.Show();*/
+        }
+
+        private void createAppointment(object sender, RoutedEventArgs e)
+        {
+            if (selectedIndex == -1)
+            {
+                MessageBox.Show("You did not selected any appointment !!!");
+                return;
+            }
+            secretaryScheduleController.NewAppointment(appointmentDtos[selectedIndex]);
+            MessageBox.Show("You created appointment");
+            this.parent.Close();
+            this.Close();
+        }
+
+        private void showEquipmentButton(object sender, RoutedEventArgs e)
+        {
+            if (selectedIndex == -1)
+            {
+                MessageBox.Show("You did not selected any appointment !!!");
+                return;
+            }
+            string serialNumber = appointmentDtos[selectedIndex].Room.SerialNumber;
             EquipmentDetails win = new EquipmentDetails(serialNumber);
             win.Show();
         }
