8d583cc0fc734505bdf038c3a203edf063ebbadf
lemara98 <mile.knezevic98@gmail.com>
2020-12-25 23:59:07 +0100

    [#GEA] Initialization of Building from DB - Updated

--- GraphicEditor/ViewModel/HospitalMapUserControlViewModel.cs
+++ GraphicEditor/ViewModel/HospitalMapUserControlViewModel.cs
@@ -6,6 +6,8 @@ using HealthClinicBackend.Backend.Model.Hospital;
 using HealthClinicBackend.Backend.Model.Util;
 using HealthClinicBackend.Backend.Repository.DatabaseSql;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -20,7 +22,7 @@ namespace GraphicEditor.ViewModel
 
         public MyICommand<string> NavCommand { get; private set; }
 
-        public MyICommand<object> AddCommand { get; private set; }
+        public MyICommand<Button> AddCommand { get; private set; }
 
         public Grid HospitalMapGrid { get; set; }
 
@@ -29,20 +31,23 @@ namespace GraphicEditor.ViewModel
             _parent = parent;
             HospitalMapGrid = hospitalMapGrid;
             NavCommand = new MyICommand<string>(ChooseHospital);
-            AddCommand = new MyICommand<object>(AddBuilding);
+            AddCommand = new MyICommand<Button>(AddBuilding);
 
-            InitialGridRender();
+            LoadBuildingsFromDatabase();
         }
 
-        /*TODO add this without causing errors
-        _viewmodel.hospitalmap.hospitalmapgrid
-        mapcontentusercontrolviewmodel.hospitalmap.hospitalmapgrid = hospitalmapgrid;
-        this.datacontext = mapcontentusercontrolviewmodel.hospitalmap;
-        mapcontentusercontrolviewmodel.hospitalmap.initialgridrender();
-        */
-
-        public void InitialGridRender()
+        public void LoadBuildingsFromDatabase()
         {
+            List<(int, int)> coordinates = new List<(int, int)>();
+
+            for (int i = 1; i < HospitalMapGrid.ColumnDefinitions.Count; i += 2)
+                for (int j = 1; j < HospitalMapGrid.RowDefinitions.Count; j += 2)
+                {
+                    if ((i == 7 && j == 3) || (i == 1 && j == 7)) continue;
+                    coordinates.Add((i, j));
+                }
+            coordinates.Remove((3, 1)); // Delete later, this position is taken by hardcoded Cardiology building!
+
             ResourceDictionary.Source = new Uri("/GraphicEditor;component/Resources/Styles/ButtonStyles.xaml", UriKind.RelativeOrAbsolute);
             foreach (Building building in buildingController.GetAll())
             {
@@ -54,21 +59,30 @@ namespace GraphicEditor.ViewModel
                 but.Background = brush;
                 Grid.SetColumn(but, building.Column);
                 Grid.SetRow(but, building.Row);
+                coordinates.Remove((building.Column, building.Row));
 
                 HospitalMapGrid.Children.Add(but);
                 HospitalMapGrid.UpdateLayout();
             }
+
+            foreach ((int i , int j)  in coordinates)
+            {
+                Button but = new Button();
+                but.Style = (Style)ResourceDictionary["NewBuildingButtonStyle"];
+                Grid.SetColumn(but, i);
+                Grid.SetRow(but, j);
+
+                HospitalMapGrid.Children.Add(but);
+                HospitalMapGrid.UpdateLayout();
+            }
+
         }
 
-        private void AddBuilding(object button)
+        private void AddBuilding(Button but)
         {
             if (MainWindow.TypeOfUser == TypeOfUser.Superintendent || MainWindow.TypeOfUser == TypeOfUser.NoUser)
             {
-                Button but = (Button)button;
-                if (but.Content.Equals("Empty field"))
-                {
-                    (new AddBuilding(but)).ShowDialog();
-                }
+                new AddBuilding(but).ShowDialog();
             }
             else
             {
