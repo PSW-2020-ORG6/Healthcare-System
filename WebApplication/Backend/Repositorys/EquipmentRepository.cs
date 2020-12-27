9f139b817f6cdbef7a7ec968826b34874c8dca2d
TanjaaD <ta.drcelic@gmail.com>
2020-12-27 16:18:44 +0100

    Change menu

--- WebApplication/Backend/Repositorys/EquipmentRepository.cs
+++ WebApplication/Backend/Repositorys/EquipmentRepository.cs
@@ -12,7 +12,7 @@ namespace WebApplication.Backend.Repositorys
 
         public EquipmentRepository()
         {
-            connection = new MySqlConnection("server=localhost;port=3306;database=mydb;user=root;password=root");
+            connection = new MySqlConnection("server=localhost;port=3306;database=mydb;user=Tanjaa;password=TanjaaD");
         }
 
         private List<Equipment> GetEquipments(String query)
