88cad478a72f7d5b9347f04f1231cd86dafe95ba
stefanwert <58173887+stefanwert@users.noreply.github.com>
2020-12-27 21:44:41 +0100

    [#GEA] Entry of data for equipment relocaion done

---
+++ GraphicEditor/View/Windows/EquipmentRelocation.xaml.cs
@@ -0,0 +1,28 @@
+﻿using GraphicEditor.ViewModel;
+using HealthClinicBackend.Backend.Model.Hospital;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GraphicEditor.View.Windows
+{
+    /// <summary>
+    /// Interaction logic for EquipmentRelocation.xaml
+    /// </summary>
+    public partial class EquipmentRelocation : Window
+    {
+        public EquipmentRelocation(Equipment equipment)
+        {
+            this.DataContext = new EquipmentRelocationViewModel(equipment);
+            InitializeComponent();
+        }
+    }
+}
