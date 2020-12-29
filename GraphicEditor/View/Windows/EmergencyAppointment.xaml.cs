b7ec04358ea62122fbd19bf93283a69468ff8ba1
Hadzija <hadzijasale@gmail.com>
2020-12-29 16:40:51 +0100

    Window for emergency examination

---
+++ GraphicEditor/View/Windows/EmergencyAppointment.xaml.cs
@@ -0,0 +1,27 @@
+﻿using GraphicEditor.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GraphicEditor.View.Windows
+{
+    /// <summary>
+    /// Interaction logic for EmergencyAppointment.xaml
+    /// </summary>
+    public partial class EmergencyAppointment : Window
+    {
+        public EmergencyAppointment(MainWindowViewModel _viewModel)
+        {
+            this.DataContext = new EmergencyAppointmentViewModel(_viewModel, this);
+            InitializeComponent();
+        }
+    }
+}
