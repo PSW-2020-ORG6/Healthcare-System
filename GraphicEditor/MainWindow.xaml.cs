e64a7f34635db46c0cbca7b8c9006b61dc918e3a
stefanwert <58173887+stefanwert@users.noreply.github.com>
2020-12-30 02:05:15 +0100

    [#GEA] Equipment relocation done

--- GraphicEditor/MainWindow.xaml.cs
+++ GraphicEditor/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using GraphicEditor.HelpClasses;
+using HealthClinicBackend.Backend.Controller;
 using HealthClinicBackend.Backend.Model.Util;
 using System;
+using System.Threading;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Threading;
@@ -13,6 +15,7 @@ namespace GraphicEditor
         public static TypeOfUser TypeOfUser = TypeOfUser.NoUser;
         public static double MainScreenWidth = SystemParameters.PrimaryScreenWidth * SCREEN_FACTOR;
         public static double MainScreenHeight = SystemParameters.PrimaryScreenHeight * SCREEN_FACTOR;
+        private EquipmentRelocationController EquipmentRelocationController = new EquipmentRelocationController();
 
         public MainWindow()
         {
@@ -22,6 +25,7 @@ namespace GraphicEditor
             this.DataContext = new MainWindowViewModel();
 
             TimeManaged();
+            TimeForEquipmentRelocation();
         }
 
         public MainWindow(TypeOfUser typeOfUser)
@@ -32,8 +36,21 @@ namespace GraphicEditor
 
             InitializeComponent();
             TimeManaged();
+            TimeForEquipmentRelocation();
+        }
+        private void ExecuteMethod(object sender, EventArgs e)
+        {
+            EquipmentRelocationController.RelocateEquipmentIfItIsTime();
+        }
+        private void TimeForEquipmentRelocation()
+        {
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += ExecuteMethod;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 15);
+            dispatcherTimer.Start();
         }
 
+        
         private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             if (this.IsActive && this.IsEnabled)
