17bb0234ed494aab5241c950a81bbc6c6480216e
Luka Doric <luka.doric@uns.ac.rs>
2021-01-08 18:16:42 +0100

    Base classes for event sourcing added.

---
+++ project-generated-code-backend/Backend/Events/Model/Event.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthClinicBackend.Backend.Events.Model
+{
+    class Event
+    {
+        public DateTime TimeStamp { get; set; }
+    }
+}
---
+++ project-generated-code-backend/Backend/Events/Repository/IEventRepository.cs
@@ -0,0 +1,12 @@
+﻿using HealthClinicBackend.Backend.Events.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthClinicBackend.Backend.Events.Repository
+{
+    interface IEventRepository
+    {
+        void LogEvent(Event @event);
+    }
+}
