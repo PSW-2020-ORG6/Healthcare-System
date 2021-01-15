e48f6916ccf567c8dddc0b5d7d208a84ae46a51c
Ana Atanackovic <anaatanackovic98@gmail.com>
2021-01-15 17:30:47 +0100

    [#GEA] Listed appointments in schedule

--- GraphicEditor/View/Windows/SchedulesWindow.xaml.cs
+++ GraphicEditor/View/Windows/SchedulesWindow.xaml.cs
@@ -1,14 +1,17 @@
 ﻿using GraphicEditor.ViewModel;
+using HealthClinicBackend.Backend.Model.Schedule;
+using System.Collections.Generic;
 using System.Windows;
 
 namespace GraphicEditor.View.Windows
 {
     public partial class SchedulesWindow : Window
     {
-        public SchedulesWindow()
+        public SchedulesWindow(List<Appointment> appointments)
         {
-            this.DataContext = new SchedulesWindowViewModel(this);
+            this.DataContext = new SchedulesWindowViewModel(appointments, this);
             InitializeComponent();
+            appointmentsListView.ItemsSource = appointments;
         }
     }
 }
--- GraphicEditor/ViewModel/SchedulesWindowViewModel.cs
+++ GraphicEditor/ViewModel/SchedulesWindowViewModel.cs
@@ -1,14 +1,19 @@
 ﻿using GraphicEditor.HelpClasses;
 using GraphicEditor.View.Windows;
+using HealthClinicBackend.Backend.Controller.SuperintendentControllers;
+using HealthClinicBackend.Backend.Model.Schedule;
+using System.Collections.Generic;
 
 namespace GraphicEditor.ViewModel
 {
     public class SchedulesWindowViewModel : BindableBase
     {
+        private AppointmentController appointmentController = new AppointmentController();
         private SchedulesWindow _window;
 
-        public SchedulesWindowViewModel(SchedulesWindow window)
+        public SchedulesWindowViewModel(List<Appointment> appointments, SchedulesWindow window)
         {
+            appointments = appointmentController.GetAll();
             _window = window;
         }
     }
