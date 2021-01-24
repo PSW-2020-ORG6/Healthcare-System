60a9e849bb73b723021196ffb901462cbc048997
Aleksandra Milijevic <aleksandra.milijevic98@uns.ac.rs>
2021-01-24 21:04:25 +0100

    Create project for selenium tests

---
+++ WebApplicationSeleniumTests/Pages/AppointmentPage.cs
@@ -0,0 +1,67 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading;
+
+namespace WebApplicationSeleniumTests.Pages
+{
+    public class AppointmentPage
+    {
+        private readonly IWebDriver driver;
+        public const string URI = "http://http://localhost:49900/#/appointments";
+        private ReadOnlyCollection<IWebElement> Rows => driver.FindElements(By.XPath("//table[@id='tableAppontments']/tbody/tr"));
+        private IWebElement CancelButton => driver.FindElement(By.XPath("(//table[@id='tableAppontments']/tbody/tr)[last()]/td[7]/button"));
+        public string Title => driver.Title;
+
+        public AppointmentPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Cancel()
+        {
+            Thread.Sleep(3000);
+            CancelButton.Click();
+        }
+
+        public void waitForCancel()
+        {
+            Thread.Sleep(1000);
+        }
+
+        public int AppointmentsCount()
+        {
+            return Rows.Count;
+        }
+
+        public bool CancelButtonEnabled()
+        {
+            return CancelButton.Enabled;
+        }
+
+        public void EnsurePageIsDisplayed()
+        {
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, 20));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    return Rows.Count > 0;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public void Navigate() => driver.Navigate().GoToUrl(URI);
+    }
+}
---
+++ WebApplicationSeleniumTests/Pages/HeaderPage.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApplicationSeleniumTests.Pages
+{
+    public class HeaderPage
+    {
+        private readonly IWebDriver driver;
+        public const string URI = "http://http://localhost:49900/#/pageHeader";
+        private IWebElement LoginButton => driver.FindElement(By.Id("loginUser"));
+
+        public HeaderPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void ClickLogInButton()
+        {
+            LoginButton.Click();
+        }
+
+        public void Navigate() => driver.Navigate().GoToUrl(URI);
+    }
+}
