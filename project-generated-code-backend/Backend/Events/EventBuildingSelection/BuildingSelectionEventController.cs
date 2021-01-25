02c094ca4770fba38f7d41f3cb6f533ee63885cf
Ana Atanackovic <anaatanackovic98@gmail.com>
2021-01-25 23:18:30 +0100

    [#GEA] Controllers for gea events

---
+++ project-generated-code-backend/Backend/Events/EventBuildingSelection/BuildingSelectionEventController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace HealthClinicBackend.Backend.Events.EventBuildingSelection
+{
+    public class BuildingSelectionEventController
+    {
+        private BuildingSelectionEventService buildingSelectionEventService;
+
+        public BuildingSelectionEventController()
+        {
+            buildingSelectionEventService = new BuildingSelectionEventService();
+        }
+
+        public void LogEvent(BuildingSelectionEventParams eventParams)
+        {
+            buildingSelectionEventService.LogEvent(eventParams);
+        }
+
+        public List<BuildingSelectionEvent> GetAll()
+        {
+            return buildingSelectionEventService.GetAll();
+        }
+    }
+}
